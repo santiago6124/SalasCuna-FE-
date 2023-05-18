08d701a659a124669b3feda9199c0c8eb253384e
Franco-Ball <francoball181@gmail.com>
2023-05-18 11:31:18 -0300

    changes in Login.js

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Login = () => (
-    <div>
-        Login
-    </div>
-);
 
-export default Login;
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        //login(email, password)
+    };
+
+    return (
+        <div className='container mt-5'>
+            <h1>Sign In</h1>
+            <p>Sign into account</p>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className='form-group'> /*email*/
+                    <input
+                        className='form-control'
+                        type='email'
+                        placeholder='Email'
+                        name='email'
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'> /*password*/
+                    <input
+                        className='form-control'
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength='6'
+                        required
+                    />
+                </div>
+                <button className='btn btn-primary' type='submit'>Login</button>
+            </form>
+            <p className='mt-3'>
+                Don't have an account? <Link to='/signup'>Sign Up</Link>
+            </p>
+            <p className='mt-3'>
+                Forgot your Password? <Link to='/reset-password'>Reset Password</Link>
+            </p>
+        </div>
+    );
+};
+
+export default connect(null, {  }) (Login);
