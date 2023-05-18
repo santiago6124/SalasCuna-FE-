7e8f3be453ebced9358492367bd435f6d6d1c8b3
Franco-Ball <francoball181@gmail.com>
2023-05-18 10:36:13 -0300

    added content in containers & App setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Activate from './containers/Activate';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+import Signup from './containers/Signup';
+
+import Layout from './hocs/Layout';
+
 const App = () => (
   <Router>
-    <Switch>
-  
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Layout>
   </Router>
 )
 
