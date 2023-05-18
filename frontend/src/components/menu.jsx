73543f365e5635c9d081ac3a449e506db3502785
JoseUrtubey <joseaurtubey@gmail.com>
2023-05-18 19:37:13 -0300

    put the logo on navbar on menu.jsx and added eliminarSala and seleccionarSala files

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import '../styles/menu.css';
-
+import '../styles/styles.css';
+import  Logo from  '../media/logo.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,7 +10,9 @@ export function Menu() {
   return (
     <Navbar expand="lg" className='navbar'>
       <Container fluid>
-        <Navbar.Brand href="#"><img src = '../media/logo.png' /></Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img src={Logo} alt=''/>
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
