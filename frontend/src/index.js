04f0edbf88376041ac39874d4320282441159231
Franco Ball <francoball181@gmail.com>
2023-05-18 16:52:53 -0300

    index changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { combineReducers } from 'redux';
+import Auth from './reducers/auth';
 
-
+export default combineReducers({
+  Auth
+});
