7c1f84bb08859dfa90d205bc899b760ae89e291c
FranciscoBrezzo <fran.brezzo@gmail.com>
2023-05-18 10:21:47 -0300

    formulario añadir domicilio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import { Button } from 'react-bootstrap';
+
 import { Menu } from './components/menu';
 import { AñadirChico } from './components/añadirChico';
 import { AñadirTutor } from './components/añadirTutor';
+import { AñadirDomicilio } from './components/añadirDomicilio';
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
       <div>
           <AñadirChico />
           <AñadirTutor />
+          <AñadirDomicilio />
+          <div className='contenedor-boton mb-5'>
+            <Button as="input" type="submit" value="Submit" size='lg' />
+          </div>
       </div>
       </body>
     </div>
