89dc2b43fc6f8e7f16b2f41c6b221a4e8d3c565e
Franco-Ball <francoball181@gmail.com>
2023-05-18 11:31:55 -0300

    incorporate store into app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Activate from './containers/Activate';
-import Home from './containers/Home';
-import Login from './containers/Login';
-import ResetPassword from './containers/ResetPassword';
-import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
-import Signup from './containers/Signup';
+import Activate from "./containers/Activate";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import ResetPassword from "./containers/ResetPassword";
+import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+import Signup from "./containers/Signup";
+import store from "./reducers/store";
 
-import Layout from './hocs/Layout';
+import Layout from "./hocs/Layout";
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/reset-password' component={ResetPassword} />
-        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-        <Route exact path='/activate/:uid/:token' component={Activate} />
-      </Switch>
-    </Layout>
-  </Router>
-)
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/reset-password" component={ResetPassword} />
+          <Route
+            exact
+            path="/password/reset/confirm/:uid/:token"
+            component={ResetPasswordConfirm}
+          />
+          <Route exact path="/activate/:uid/:token" component={Activate} />
+        </Switch>
+      </Layout>
+    </Router>
+  </Provider>
+);
 
 export default App;
