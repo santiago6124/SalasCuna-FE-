c046edde7f991b90090a6d89e7a16b166da86568
Franco Ball <francoball181@gmail.com>
2023-05-19 17:54:43 -0300

    added checkAuth function

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -3,8 +3,47 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     USER_LOADED_SUCCESS,
-    USER_LOADED_FAIL
+    USER_LOADED_FAIL,
+    AUTHENTICATED_SUCCESS,
+    AUTHENTICATED_FAIL,
+    LOGOUT
 } from './types';
+
+export const checkAuthenticated = () => async dispatch => {
+    if (localStorage.getItem('access')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        };
+
+        const body = JSON.stringify({ token: localStorage.getItem('access') });
+
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/verify`, body, config);
+
+            if (res.data.code !== 'token_not_valid') {
+                dispatch({
+                    type: AUTHENTICATED_SUCCESS
+                });
+            } else {
+                dispatch({
+                    type: AUTHENTICATED_FAIL
+                });
+            }
+        } catch (err){
+            dispatch({
+                type: AUTHENTICATED_FAIL
+            });
+        };
+
+    } else {
+        dispatch({
+            type: AUTHENTICATED_FAIL
+        });
+    }
+};
 
 export const load_user = () => async dispatch =>{
     if (localStorage.getItem('access')) {
@@ -60,3 +99,8 @@ export const login = (email, password) => async dispatch => {
     }
 };
 
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    });
+};
