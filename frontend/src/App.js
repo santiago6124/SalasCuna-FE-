1694593b35c01c8b8e833adca08010a31d37df6a
FranciscoBrezzo <fran.brezzo@gmail.com>
2023-05-30 08:38:56 -0300

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 
+import { Button } from 'react-bootstrap';
+
+import { Menu } from './components/menu';
+import { AñadirChico } from './components/añadirChico';
+import { AñadirTutor } from './components/añadirTutor';
+import { AñadirDomicilio } from './components/añadirDomicilio';
+
 
 function App() {
   return (
     <div className="App">
       <header>
+        <div>
+          <Menu />
+        </div>
       </header>
-      
+      <body className='body'>
+        <div>
+            <AñadirChico />
+            <AñadirTutor />
+            <AñadirDomicilio />
+            <div className='contenedor-boton mb-5'>
+              <Button as="input" type="submit" value="Cargar" size='lg' />
+            </div>
+        </div>
+      </body>
     </div>
   );
 }
