58d5435458b6fde1918b34aad6aa00333c55539b
FranciscoBrezzo <fran.brezzo@gmail.com>
2023-05-31 11:25:34 -0300

    .

---
+++ frontend/src/components/DeleteChildren/DeleteChildren.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../DeleteChildren/DeleteChildren.css';
+
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+
+import { FaQrcode } from 'react-icons/fa';
+
+
+export function DeleteChildren() {
+  return (
+    <Form className='conteiner-form'>
+        <h1 className='titulo'>Dar Baja Niños/as</h1>
+        
+        <div className='contenedor-linea'>
+            <hr className='linea' ></hr>
+        </div>
+        <Form.Group className="mb-3 mt-5" >
+            <Form.Label className='mb-1'><b>Nombre</b></Form.Label>
+            <Form.Control type="text" placeholder="Ingrese un nombre" />
+        </Form.Group>
+            
+        <div className='contenedor-boton-qr '>
+            <Button className='boton-qr mt-5' boton variant="primary">
+                Eliminar
+            </Button>
+        </div>
+
+    </Form>
+  );
+}
