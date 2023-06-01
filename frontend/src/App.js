9cbefd1361665eabc6d218e0700f71e0042a36ea
Franco-Ball <francoball181@gmail.com>
2023-06-01 11:25:57 -0300

    changes in home && App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Login from "./containers/Login";
 import ResetPassword from "./containers/ResetPassword";
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
 import Signup from "./containers/Signup";
+import Google from './containers/Google';
 import store from "./store";
 
 import Layout from "./hocs/Layout";
@@ -17,16 +18,13 @@ const App = () => (
     <Router>
       <Layout>
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/reset-password" component={ResetPassword} />
-          <Route
-            exact
-            path="/password/reset/confirm/:uid/:token"
-            component={ResetPasswordConfirm}
-          />
-          <Route exact path="/activate/:uid/:token" component={Activate} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/google' component={Google} />
+        <Route exact path='/reset-password' component={ResetPassword} />
+        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+        <Route exact path='/activate/:uid/:token' component={Activate} />
         </Routes>
       </Layout>
     </Router>
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Home = () => (
-    <div>
-        Home
+    <div className='container'>
+        <div class='jumbotron mt-5'>
+            <h1 class='display-4'>Welcome to Auth System!</h1>
+            <p class='lead'>This is an incredible authentication system with production level features!</p>
+            <hr class='my-4' />
+            <p>Click the Log In button</p>
+            <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+        </div>
     </div>
 );
 
