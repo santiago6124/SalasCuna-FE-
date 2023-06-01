6497b0212d5c0a63768b659fad2eb45d4adc5a66
Franco-Ball <francoball181@gmail.com>
2023-06-01 10:13:09 -0300

    googleoauth2 function

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -22,7 +22,7 @@ const Login = ({ login, isAuthenticated }) => {
 
     const continueWithGoogle = async () => {
         try {
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/google-oauth2/?redirect_uri=http://localhost:8000`)
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/google-oauth2/?redirect_uri=${process.env.REACT_APP_API_URL}/google`)
 
             window.location.replace(res.data.authorization_url);
         } catch (err) {
