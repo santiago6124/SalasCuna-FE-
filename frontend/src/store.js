98e9bd983d250bd637a858cf3d291e1ec39118e6
santiago6124 <santiagocarranzazinny@gmail.com>
2023-06-01 11:22:22 -0300

    FACUOLIVAMAR fixed error of importing component.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import {applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers"
+import rootReducer from "../src/reducers/auth";
 import { legacy_createStore as createStore } from 'redux';
 
 const initialState = {};
