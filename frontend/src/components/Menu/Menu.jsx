0130db15feede95ba020ef9a83910974fb53c0bc
Franco-Ball <francoball181@gmail.com>
2023-06-01 10:46:54 -0300

    working on navbar

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth';
 
 import '../Menu/Menu.css';
 import logo from '../../media/logo.png';
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
-export function Menu() {
+const Menu = ({logout, isAuthenticated}) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const logout_user = () => {
+      logout();
+      setRedirect(true);
+  };
+
+  const guestLinks = () => (
+      <Fragment>
+          <li className='nav-item'>
+              <Link className='nav-link' to='/login'>Login</Link>
+          </li>
+          <li className='nav-item'>
+              <Link className='nav-link' to='/signup'>Sign Up</Link>
+          </li>
+      </Fragment>
+  );
+
+  const authLinks = () => (
+      <li className='nav-item'>
+          <a className='nav-link' href='#!' onClick={logout_user}>Logout</a>
+      </li>
+  );
+
+
   return (
     <Navbar expand="lg" className='navbar'>
       <Container fluid>
         <Navbar.Brand href="#">
-          <img src={ logo } alt="" className='logo'/>
+          <img src={ logo } alt="logo de" className='logo'/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -35,8 +62,20 @@ export function Menu() {
             <Nav.Link to='../pages/padronPage.jsx'>Padron</Nav.Link>
             <Nav.Link to='../pages/presupuestoPage.jsx'>Presupuesto</Nav.Link>
           </Nav>
+
+          {isAuthenticated ? authLinks() : guestLinks()}
+
         </Navbar.Collapse>
       </Container>
+
+      {redirect ? <Redirect to='/' /> : <Fragment></Fragment>}
+      
     </Navbar>
   );
 }
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logout })(Menu);
