fa766adc86950617bb42c1d9c67a8953064ac2dc
JoseUrtubey <joseaurtubey@gmail.com>
2023-06-06 10:16:07 -0300

    changing pc to carran's

---
+++ frontend/src/components/CreateUser/CreateUser.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap";
+
+import "./CreateUser.css";
+
+export function CreateUser() {
+    return(
+        <Form className="container-form-createuser">
+            <h1 className="titulo-createuser">Crear Usuario</h1>
+
+            <div><h1 className="linea-container-createuser"></h1></div>
+            <Form.Group>
+                
+            </Form.Group>
+        </Form>
+    );
+}
