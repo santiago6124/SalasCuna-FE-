a51a0ae84b1155f14fc6cf66a97b8fd1ab3302dc
santiago6124 <santiagocarranzazinny@gmail.com>
2023-06-06 11:58:54 -0300

    App and login with problems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import Activate from "./containers/Activate";
-import Home from "./containers/Home";
-import Login from "./containers/Login";
-import ResetPassword from "./containers/ResetPassword";
-import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
-import Signup from "./containers/Signup";
-import Google from './containers/Google';
-import store from "./store";
 
-import Layout from "./hocs/Layout";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Layout>
-        <Routes>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/google' component={Google} />
-        <Route exact path='/reset-password' component={ResetPassword} />
-        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-        <Route exact path='/activate/:uid/:token' component={Activate} />
-        </Routes>
-      </Layout>
-    </Router>
-  </Provider>
-);
+import './App.css';
+
+//import { Button } from 'react-bootstrap';
+
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      </Routes>
+  );
+}
 
 export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import Col from "react-bootstrap/Col/";
+import Row from "react-bootstrap/Row/";
+import Form from "react-bootstrap/Form/";
+import { Button } from "react-bootstrap";
+
+import { FaQrcode } from "react-icons/fa";
+
+const Login = () => {
+  return (<h1>Hola</h1>)
+}
+
+export default Login
