bbc8da01f20fce9a979f57a5a3cc4bd11a2017a0
Franco Ball <francoball181@gmail.com>
2023-06-21 17:36:22 -0300

    changes in app & deleted google

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
+import { AuthProvider } from './context/AuthContext';
+import './App.css';
+import Navbar from './components/Menu/Menu';
 
 import Activate from "./containers/Activate";
 import Home from "./containers/Home";
@@ -8,27 +10,27 @@ import Login from "./containers/Login";
 import ResetPassword from "./containers/ResetPassword";
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
 import Signup from "./containers/Signup";
-import Google from './containers/Google';
-import store from "./store";
 
-import Layout from "./hocs/Layout";
 
-const App = () => (
-  <Provider store={store}>
+function App() {
+  return (
     <Router>
-      <Layout>
-        <Routes>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/google' component={Google} />
-        <Route exact path='/reset-password' component={ResetPassword} />
-        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-        <Route exact path='/activate/:uid/:token' component={Activate} />
-        </Routes>
-      </Layout>
+      <div className='App'>
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/reset-password' component={ResetPassword} />
+            <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+            <Route exact path='/activate/:uid/:token' component={Activate} />
+          </Routes>
+        </AuthProvider>
+      </div>
+
     </Router>
-  </Provider>
-);
+  );
+}
 
 export default App;
