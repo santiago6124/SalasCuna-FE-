5723974c0cc76105645e38a67ac8de702ed447de
Franco Ball <francoball181@gmail.com>
2023-06-21 20:19:22 -0300

    fix imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
 import Navbar from './components/Menu/Menu';
+import PrivateRoute from "./utils/PrivateRoute";
+
 
 import Activate from "./containers/Activate";
 import Home from "./containers/Home";
@@ -10,6 +12,7 @@ import Login from "./containers/Login";
 import ResetPassword from "./containers/ResetPassword";
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
 import Signup from "./containers/Signup";
+import LoginPage from "./components/login";
 
 
 function App() {
@@ -19,16 +22,15 @@ function App() {
         <AuthProvider>
           <Navbar />
           <Routes>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/reset-password' component={ResetPassword} />
-            <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-            <Route exact path='/activate/:uid/:token' component={Activate} />
+            <Route path='/' exact element={Home} />
+            <Route path='/login' exact element={<LoginPage/>} />
+            <Route path='/signup' component={Signup} />
+            <Route path='/reset-password' component={ResetPassword} />
+            <Route path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+            <Route path='/activate/:uid/:token' component={Activate} />
           </Routes>
         </AuthProvider>
       </div>
-
     </Router>
   );
 }
