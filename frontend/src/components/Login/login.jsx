a26060db40415000aa522759726dbe5a03f85179
Franco Ball <francoball181@gmail.com>
2023-06-22 20:48:04 -0300

    moved loginpage

--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Button, Container, FloatingLabel, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../../context/AuthContext";
 
 const LoginPage = () => {
   return (
