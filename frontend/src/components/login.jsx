359954d84d17c23af05a02d2b3ff1e2b7f334442
Franco Ball <francoball181@gmail.com>
2023-06-25 17:34:22 -0300

    ???

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,41 +1,12 @@
-import React, { useContext } from "react";
-import { Button, Container, FloatingLabel, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
+import Col from "react-bootstrap/Col/";
+import Row from "react-bootstrap/Row/";
+import Form from "react-bootstrap/Form/";
+import { Button } from "react-bootstrap";
 
-const LoginPage = () => {
-  let {loginUser} = useContext(AuthContext)
-  return (
-    <Container fluid className="conteiner-form-login">
-      <Form onSubmit={loginUser}>
-        <h1 className="titulo">Iniciar Sesion</h1>
+import { FaQrcode } from "react-icons/fa";
 
-        <div className="contenedor-linea">
-          <hr className="linea"></hr>
-        </div>
-        <Form.Group className="mb-3">
-          <Form.Label className="mb-1">Usuario</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su nombre de usuario" name="username" required/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="mb-1">Contraseña</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su contraseña" name="password" minLength='8' required/>
-        </Form.Group>
-        <div className="contenedor-boton-createuser">
-        <Button className="boton mt-3" boton variant="primary" type="submit">Log in</Button>
-      </div>
-      </Form>
-      <Link to="/signup/">
-        <Button variant="link" className="link">
-          No tengo una cuenta
-        </Button>
-      </Link>
-      <Link to="/reset-password/">
-        <Button variant="link" className="link">
-          Me olvidé la contraseña
-        </Button>
-      </Link>
-    </Container>
-  );
-};
-export default LoginPage;
+const Login = () => {
+  return (<h1>Hola</h1>)
+}
+
+export default Login
