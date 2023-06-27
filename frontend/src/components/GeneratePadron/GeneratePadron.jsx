722d2acb8026a6b394b9c96ea767546e5dfecd81
JoseUrtubey <joseaurtubey@gmail.com>
2023-06-26 21:17:39 -0300

    I THINK IT WORKS

--- frontend/src/components/GeneratePadron/GeneratePadron.jsx
+++ frontend/src/components/GeneratePadron/GeneratePadron.jsx
@@ -1,8 +1,5 @@
 import "./GeneratePadron.css";
-
-
-import Table from "react-bootstrap/Table/";
-
+import Table from "react-bootstrap/Table";
 import React, { useEffect, useState } from "react";
 
 const GeneratePadron = () => {
@@ -13,6 +10,7 @@ const GeneratePadron = () => {
       try {
         const response = await fetch("http://127.0.0.1:8000/api/child/");
         const jsonData = await response.json();
+        console.log(jsonData); // Check the fetched data in the console
         setChild(jsonData);
       } catch (error) {
         console.error("Error", error);
@@ -24,37 +22,44 @@ const GeneratePadron = () => {
 
   // Render the data in a table
   return (
-      <div className="container-note mt-5">
-      <div className="container-titulo-note ">
+    <div className="container-note mt-5">
+      <div className="container-titulo-note">
         <h1>Generar Padron</h1>
       </div>
       <div className="contenedor-linea-note">
         <hr className="linea-note"></hr>
       </div>
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Apellido</th>
-          <th>Nombre</th>
-          <th>N° DNI</th>
-          <th> Fecha de Nacimiento</th>
-          <th>Edad</th>
-          <th>Sexo</th>
-        </tr>
-      </thead>
-      <tbody>
-        {child.map((child) => (
-          <tr key={child.id}>
-            <td>{child.last_name}</td>
-            <td>{child.first_name}</td>
-            <td>{child.dni}</td>
-            <td>{child.birthdate}</td>
-            <td>{child.age}</td>
-            <td>{child.sex}</td>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Apellido</th>
+            <th>Nombre</th>
+            <th>N° DNI</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Edad</th>
+            <th>Sexo</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {(() => {
+            const rows = [];
+            for (let i = 0; i < child.length; i++) {
+              const child = child[i];
+              rows.push(
+                <tr key={child.id}>
+                  <td>{child.last_name}</td>
+                  <td>{child.first_name}</td>
+                  <td>{child.dni}</td>
+                  <td>{child.birthdate}</td>
+                  <td>{child.age}</td>
+                  <td>{child.sex}</td>
+                </tr>
+              );
+            }
+            return rows;
+          })()}
+        </tbody>
+      </Table>
     </div>
   );
 };
