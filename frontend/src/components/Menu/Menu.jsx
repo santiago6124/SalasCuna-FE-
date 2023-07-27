d45f7145528e46de481c17c8136f9bbe18c84d00
santiago6124 <santiagocarranzazinny@gmail.com>
2023-07-27 09:51:24 -0300

    use of prompts in menu

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,56 +1,48 @@
-import React, {useContext} from 'react';
-import AuthContext from '../../context/AuthContext';
-
+import React from 'react';
 import './Menu.css';
 import logo from '../../media/logo.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+const Menu = ({ user, logoutUser, link1Text, link1Path, link2Text, link2Path }) => {
+  return (
+    <Navbar expand="lg" className="navbar">
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="logo de" className="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Link to={link1Path}>
+              <Nav className="titulo">{link1Text}</Nav>
+            </Link>
+            <Link to={link2Path}>
+              <Nav className="titulo">{link2Text}</Nav>
+            </Link>
 
-const Menu = () => {
-    let {user, logoutUser} = useContext(AuthContext);
-    return (
-        <Navbar expand="lg" className='navbar'>
-            <Container fluid>
-                <Navbar.Brand href="/">
-                    <img src={logo} alt="logo de" className='logo'/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{maxHeight: '100px'}}
-                        navbarScroll
-                    >
-                        
-                        <Link to='/añadir-chico'>
-                            <Nav className='titulo'>Añadir Chico</Nav>
-                        </Link>
-                        <Link to='/generate-padron'>
-                            <Nav className='titulo'>Generar Padron</Nav>
-                        </Link>
-                        
-                        {user ? (
-                                <Link to='/'>
-                                    <Button className="boton" boton variant="primary" onClick={logoutUser}>Log Out</Button>
-                                </Link>
-                            ) : (
-                                <Link to='/login'>
-                                    <Button className="boton" boton variant="primary">Log In</Button>
-                                </Link>
-                            )
-                        }
-
-                    </Nav>
-
-                </Navbar.Collapse>
-            </Container>
-
-        </Navbar>
-    );
+            {user ? (
+              <Link to="/">
+                <Button className="boton" variant="primary" onClick={logoutUser}>
+                  Log Out
+                </Button>
+              </Link>
+            ) : (
+              <Link to="/login">
+                <Button className="boton" variant="primary">
+                  Log In
+                </Button>
+              </Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
 
 export default Menu;
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,19 +1,29 @@
 import {React, useContext} from 'react';
 import {Link} from 'react-router-dom'
 import AuthContext from '../context/AuthContext';
+import Menu from './Menu';
 
 const Home = () => {
     const {user} = useContext(AuthContext);
-
     return (
-        <div className='container'>
+        <>
+          <Menu
+            user={user}
+            logoutUser={logoutUser}
+            link1Text="Custom Link 1"
+            link1Path="/custom-link-1"
+            link2Text="Custom Link 2"
+            link2Path="/custom-link-2"
+          />
+                  <div className='container'>
             <div class='jumbotron mt-5'>
                 <h1 class='display-4'>Bienvenido a Salas Cuna</h1>
                 <p class='lead'>Esta version del sistema permite registrar un usuario, ingresar con el usuario creado, añadir un chico a la base de datos y ver el padron.</p>
                 <hr class='my-4'/>
             </div>
         </div>
-    )
-};
+        </>
+      );
+    };
 
 export default Home;
