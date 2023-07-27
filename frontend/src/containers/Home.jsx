53d5b6c26387c0ddc48c37ea19c076593161113b
santiago6124 <santiagocarranzazinny@gmail.com>
2023-07-27 09:52:46 -0300

    hotfix in home

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
           />
                   <div className='container'>
             <div class='jumbotron mt-5'>
-                <h1 class='display-4'>Bienvenido a Salas Cuna</h1>
+                <h1 class='display-4'>Bienvenido a SalasCuna</h1>
                 <p class='lead'>Esta version del sistema permite registrar un usuario, ingresar con el usuario creado, añadir un chico a la base de datos y ver el padron.</p>
                 <hr class='my-4'/>
             </div>
