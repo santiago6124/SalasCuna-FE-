929c626aefb38e03a8df13456df52246a069b72b
JoseUrtubey <joseaurtubey@gmail.com>
2023-07-29 12:01:38 -0300

    Login form fields completed

--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,41 +1,160 @@
 import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 
 import "./Login.css";
 
 const LoginPage = () => {
   let { loginUser } = useContext(AuthContext);
+
   return (
     <div className="body">
       <div>
         <Container fluid className="conteiner-form-login">
           <Form onSubmit={loginUser}>
             <h1 className="titulo">Iniciar Sesion</h1>
-
             <div className="contenedor-linea">
               <hr className="linea"></hr>
             </div>
+            {/* Email Label */}
             <Form.Group className="mb-3">
-              <Form.Label className="mb-1">Usuario</Form.Label>
+              <Form.Label className="mb-1">E-mail</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Ingrese su nombre de usuario"
-                name="username"
+                placeholder="Ingresar E-mail"
+                name="email"
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="mb-1">Contraseña</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Ingrese su contraseña"
-                name="password"
-                minLength="8"
-                required
-              />
-            </Form.Group>
+            <div className="form-row">
+              {/* Nombre Label */}
+              <div className="col-md-6 mb-3">
+                <Form.Label className="mb-1">Nombre</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar nombre"
+                  name="first_name"
+                  required
+                />
+              </div>
+
+              {/* Apellido Label */}
+              <div className="col-md-6 mb-3">
+                <Form.Label className="mb-1">Apellido</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar Apellido"
+                  name="last_name"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              {/* DNI  Label*/}
+              <div className="col-md-6 mb-3">
+                <Form.Label className="mb-1">DNI</Form.Label>
+                <Form.Control
+                  type="int"
+                  placeholder="Ingresar El Dni"
+                  name="dni"
+                  required
+                />
+              </div>
+
+              {/* Rol label (Gotta change to dropdown later) */}
+              <div className="col-md-6 mb-3">
+                <Form.Label className="mb-1">Rol</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar Rol del trabajador"
+                  name="role"
+                  required
+                />
+              </div>
+            </div>
+            
+            {/*Phone number Label*/}
+            <div className="Form-Control">
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1 ">Numero De Telefono</Form.Label>
+                <Form.Control
+                  className="form-label"
+                  type="int"
+                  placeholder="Ingresar Nro De Telefono"
+                  name="phone_number"
+                  required
+                />
+              </Form.Group>
+            </div>
+            
+            <div className="Form-Control">
+              {/*Address Label*/}
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1">Direccion</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar Direccion"
+                  name="address"
+                  required
+                />
+              </Form.Group>
+            </div>
+            
+            {/*Department Label*/}
+            <div className="Form-Control">
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1">Departamento</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar Departamento"
+                  name="department"
+                  required
+                />
+              </Form.Group>
+            </div>
+           
+            <div className="Form-Control">
+              {/*City Label*/}
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1">Ciudad</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar Ciudad"
+                  name="city"
+                  required
+                />
+              </Form.Group>
+            </div>
+
+            <div className="Form-Control">
+              {/*Password Label*/}
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1">Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                  name="password"
+                  minLength="8"
+                  required
+                />
+              </Form.Group>
+            </div>
+
+            <div className="Form-Control">
+              {/*Repeat Password Label*/}
+              <Form.Group className="mb-3">
+                <Form.Label className="mb-1">Repetir Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese su contraseña de nuevo"
+                  name="re_password"
+                  minLength="8"
+                  required
+                />
+              </Form.Group>
+            </div>
+
             <div className="contenedor-boton-createuser">
               <Button
                 className="boton mt-3"
@@ -43,23 +162,14 @@ const LoginPage = () => {
                 variant="primary"
                 type="submit"
               >
-                Log in
+                Ingresar
               </Button>
             </div>
           </Form>
-          <Link to="/signup/">
-            <Button variant="link" className="link">
-              No tengo una cuenta
-            </Button>
-          </Link>
-          <Link to="/reset-password/">
-            <Button variant="link" className="link">
-              Me olvidé la contraseña
-            </Button>
-          </Link>
         </Container>
       </div>
     </div>
   );
 };
+
 export default LoginPage;
