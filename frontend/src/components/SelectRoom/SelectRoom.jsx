1d82ee1b82fb92953c52f69f2ab713db2b722653
Franco-Ball <francoball181@gmail.com>
2023-08-07 17:42:00 -0300

    added Cribrooms to Dropdown

--- frontend/src/components/SelectRoom/SelectRoom.jsx
+++ frontend/src/components/SelectRoom/SelectRoom.jsx
@@ -4,9 +4,26 @@ import Row from "react-bootstrap/Row/";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export function SelectRoom() {
+    const [cribroom, setCribroom] = useState([]);
+
+    useEffect(() =>{
+        LoadCribrooms();
+    });
+    let LoadCribrooms = async () => {
+        try {
+            let response = await fetch('http://127.0.0.1:8000/api/cribroom/');
+            let jsonData = await response.json();
+            console.log(jsonData);
+            setCribroom(jsonData);
+
+        } catch (error) {
+            console.error("Error", error);
+        }
+    };
+
     return (
         <Form className="conteiner-form-select">
             <h1 className="titulo-select">Editar Sala Cuna</h1>
@@ -20,9 +37,11 @@ export function SelectRoom() {
                         Seleccionar Sala Cuna Que Desea Editar
                     </Form.Label>
                     <Form.Select placeholder="Seleccionar Sala Cuna" className="mb-1">
-                        <option value="Sala Cuna 1">Sala Cuna 1</option>
-                        <option value="Sala Cuna 2">Sala Cuna 2</option>
-                        <option value="Sala Cuna 3">Sala Cuna 3</option>
+                        {cribroom.map((room) => (
+                            <option key={room.id} value={room.id}>
+                                {room.name}
+                            </option>
+                        ))}
                     </Form.Select>
                     <div className="contenedor-boton-select">
                         <Button className="button-select mt-4" boton variant="primary">
