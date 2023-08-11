7e091c4648f42406764fd1b6220dd8f92be89abe
Franco-Ball <francoball181@gmail.com>
2023-08-11 11:15:30 -0300

    Created CribroomDashboard files

---
+++ frontend/src/components/CribroomDashboard/CribroomDashboard.jsx
@@ -0,0 +1,16 @@
+import "../CribroomDashboard.css"
+
+import { getAllCribrooms, getAllShifts, getAllZones } from "../../api/salasCuna.api"
+
+
+export function CribroomDashboard() {
+
+
+
+
+
+
+    return(
+        <div className="cribroom-dashboard"></div>
+    )
+}
