83c76222f977e7c69704b07a4058c32229fbef29
Franco-Ball <francoball181@gmail.com>
2023-08-11 11:15:11 -0300

    added get Zone & Shift

--- frontend/src/api/salasCuna.api.js
+++ frontend/src/api/salasCuna.api.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const getAllCribrooms = () => {
   return axios.get('http://127.0.0.1:8000/api/cribroom/');
 };
+
+export const getAllZones = () => {
+  return axios.get('http://127.0.0.1:8000/api/zone/');
+};
+
+export const getAllShifts = async () => {
+  return axios.get('http://127.0.0.1:8000/api/shift/');
+};
