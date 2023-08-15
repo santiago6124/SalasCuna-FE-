2fe0aac114dab7b0e7cc3538f19cd41975183841
santiago6124 <santiagocarranzazinny@gmail.com>
2023-08-15 10:01:31 -0300

    payout functions ready :)

--- frontend/src/components/Payout/Payout.jsx
+++ frontend/src/components/Payout/Payout.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import "../AddChildren/AddChildren.css";
 
 import Col from "react-bootstrap/Col/";
 import Row from "react-bootstrap/Row/";
@@ -15,7 +14,36 @@ export default function Payout() {
   const [selectedZona, setSelectedZone] = useState("");
   const [zone1Payouts, setZone1Payouts] = useState([]);
   const [zone2Payouts, setZone2Payouts] = useState([]);
+  const [editedPayout, setEditedPayout] = useState(null);
+  const handleEditClick = (payout) => {
+    setEditedPayout(payout);
+  };
+  const handleEditSubmit = async (event) => {
+    event.preventDefault();
 
+    const formData = new FormData(event.target);
+    const updatedPayout = {
+      amount: formData.get("amount"),
+      date: formData.get("date"),
+    };
+
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/payout/${editedPayout.id}/`, // Use the correct endpoint with the primary key
+        updatedPayout
+      );
+    
+      if (response.status === 200) {
+        console.log("Payout updated successfully");
+        // Refresh the UI or update the payouts state as needed
+        setEditedPayout(null); // Clear the editedPayout state
+      } else {
+        console.log("Failed to update payout");
+      }
+    } catch (error) {
+      console.error("An error occurred while updating payout:", error);
+    }
+  };
   useEffect(() => {
     loadZones();
     loadZone1Payouts();
@@ -23,7 +51,7 @@ export default function Payout() {
   }, []);
   const loadZone1Payouts = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/payout/?zone=1");
+      const response = await fetch("http://127.0.0.1:8000/api/payout/?zone_id_filter=1");
       const jsonData = await response.json();
       setZone1Payouts(jsonData);
     } catch (error) {
@@ -33,7 +61,7 @@ export default function Payout() {
   
   const loadZone2Payouts = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/payout/?zone=2");
+      const response = await fetch("http://127.0.0.1:8000/api/payout/?zone_id_filter=2");
       const jsonData = await response.json();
       setZone2Payouts(jsonData);
     } catch (error) {
@@ -43,7 +71,7 @@ export default function Payout() {
 
   const loadZones = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/zone/");
+      const response = await fetch("http://127.0.0.1:8000/api/ZoneReadOnlyModelViewSet/");
       let jsonData = await response.json();
       setZoneOptions(jsonData);
     } catch (error) {
@@ -89,59 +117,78 @@ export default function Payout() {
     <div>
       <h1>Add Payout</h1>
       <form onSubmit={handleSubmit}>
-        <label>Amount:</label>
-        <input type="number" name="amount" required />
-        <br />
-        <label>Date:</label>
-        <input type="date" name="date" required />
-        <br />
-        <Form.Label className="mb-1">Zona</Form.Label>
-        <Form.Select
-  name="zoneCR"
-  as="select"
-  value={selectedZona}
-  onChange={handleSelectChange}
->
-  <option value="" disabled>
-    Seleccionar Zona
-  </option>
-  {zoneOptions.map((zone) => (
-    <option key={zone.id} value={zone.id}>
-      {zone.name}
-    </option>
-  ))}
-</Form.Select>
-        <br />
+        {/* ... Existing form fields */}
         <button type="submit">Add Payout</button>
       </form>
+  
+      {editedPayout && (
+        <div>
+          <h2>Edit Payout</h2>
+          <form onSubmit={handleEditSubmit}>
+            <label>Amount:</label>
+            <input
+              type="number"
+              name="amount"
+              defaultValue={editedPayout.amount}
+              required
+            />
+            <br />
+            <label>Date:</label>
+            <input
+              type="date"
+              name="date"
+              defaultValue={editedPayout.date}
+              required
+            />
+            <br />
+            <button type="submit">Update Payout</button>
+          </form>
+        </div>
+      )}
+  
       <div>
-  <h2>Zone 1 Payouts</h2>
-  <DataGrid
-    rows={zone1Payouts}
-    columns={[
-      { field: 'id', headerName: 'ID', width: 70 },
-      { field: 'amount', headerName: 'Amount', width: 130 },
-      { field: 'date', headerName: 'Date', width: 130 },
-    ]}
-    autoHeight
-    pageSize={5}
-  />
-</div>
-<div>
-  <h2>Zone 2 Payouts</h2>
-  <DataGrid
-    rows={zone2Payouts}
-    columns={[
-      { field: 'id', headerName: 'ID', width: 70 },
-      { field: 'amount', headerName: 'Amount', width: 130 },
-      { field: 'date', headerName: 'Date', width: 130 },
-    ]}
-    autoHeight
-    pageSize={5}
-  />
-</div>
+        <h2>Zone 1 Payouts</h2>
+        <DataGrid
+          rows={zone1Payouts}
+          columns={[
+            { field: 'id', headerName: 'ID', width: 70 },
+            { field: 'amount', headerName: 'Amount', width: 130 },
+            { field: 'date', headerName: 'Date', width: 130 },
+            {
+              field: 'actions',
+              headerName: 'Actions',
+              width: 120,
+              renderCell: (params) => (
+                <Button onClick={() => handleEditClick(params.row)}>Edit</Button>
+              ),
+            },
+          ]}
+          autoHeight
+          pageSize={5}
+        />
+      </div>
+      <div>
+        <h2>Zone 2 Payouts</h2>
+        <DataGrid
+          rows={zone2Payouts}
+          columns={[
+            { field: 'id', headerName: 'ID', width: 70 },
+            { field: 'amount', headerName: 'Amount', width: 130 },
+            { field: 'date', headerName: 'Date', width: 130 },
+            {
+              field: 'actions',
+              headerName: 'Actions',
+              width: 120,
+              renderCell: (params) => (
+                <Button onClick={() => handleEditClick(params.row)}>Edit</Button>
+              ),
+            },
+          ]}
+          autoHeight
+          pageSize={5}
+        />
+      </div>
     </div>
-    
   );
 }
 
