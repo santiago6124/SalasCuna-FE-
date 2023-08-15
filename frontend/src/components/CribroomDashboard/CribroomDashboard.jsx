a783f61692b1f767ee2fb760eb334ff90e45008d
Franco-Ball <francoball181@gmail.com>
2023-08-15 11:58:22 -0300

    Fixed file

--- frontend/src/components/CribroomDashboard/CribroomDashboard.jsx
+++ frontend/src/components/CribroomDashboard/CribroomDashboard.jsx
@@ -18,137 +18,66 @@ export default function CribroomDashboard() {
   const [selectedCribroom, setSelectedCribroom] = useState("");
 
   useEffect(() => {
-    ListCribroom();
-  }, []);
+      listCribroom();
+    }, []);
 
-  const ListCribroom = async () => {
-    try {
-      const response = await getAllCribrooms();
-      setCribrooms(response.data);
-    } catch (error) {
-      console.log("Error fetching SalasCunas:", error);
-    }
-  };
-
-  const ListShift = async () => {
-    try {
-      const response = await getAllShifts();
-      setShiftOptions(response.data);
-    } catch (error) {
-      console.log("Error fetching Shifts:", error);
-    }
-  };
-
-  const ListZone = async () => {
-    try {
-      const response = await getAllZones();
-      setZoneOptions(response.data);
-    } catch (error) {
-      console.log("Error fetching Zones:", error);
-    }
-  };
-
-  const handleEdit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const payload = {
-      name: formData.get("nameCR"),
-      code: formData.get("codeCR"),
-      max_capacity: formData.get("max_capacityCR"),
-      street: formData.get("streetCR"),
-      house_number: formData.get("house_numberCR"),
-      shift: formData.get("shiftCR"),
-      zone: formData.get("zoneCR"),
-    };
-
-    if (selectedCribroom) {
+  const listCribroom = async () => {
       try {
-        let response = await fetch(
-          "http://127.0.0.1:8000/api/cribroom/" + selectedCribroom + "/",
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
+        const response = await getAllCribrooms();
+        setCribrooms(response.data);
+      } catch (error) {
+        console.log('Error fetching SalasCunas:', error);
+      }
+    };
+    
+  const listShift = async () => {
+      try {
+        const response = await getAllShifts();
+        setShiftOptions(response.data);
+      } catch (error) {
+        console.log('Error fetching Shifts:', error);
+      }
+    };
+  const listZone = async () => {
+      try {
+        const response = await getAllZones();
+        setZoneOptions(response.data);
+      } catch (error) {
+        console.log('Error fetching Zones:', error);
+      }
+  };
+  const handleEdit = async (event) => {
+      event.preventDefault()
+      const formData = new FormData(event.target);
+      const payload = {
+          name: formData.get("nameCR"),
+          code: formData.get("codeCR"),
+          max_capacity: formData.get("max_capacityCR"),
+          street: formData.get("streetCR"),
+          house_number: formData.get("house_numberCR"),
+          shift: formData.get("shiftCR"),
+          zone: formData.get("zoneCR"),
+      };
+      if(selectedCribroom){
+          try {
+              let response = await fetch('http://127.0.0.1:8000/api/cribroom/'+ selectedCribroom +'/', {
+                  method: 'PUT',
+                  headers: {
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(payload),
+              });
+              if (response.ok){
+                  console.log('Cribroom Updated')
+              } else {
+                  console.log('Failed to Update')
+              }
+          } catch (err) {
+              alert(err)
           }
-        );
-
-        if (response.ok) {
-          console.log("Cribroom Updated");
-        } else {
-          console.log("Failed to Update");
         }
-      } catch (err) {
-        alert(err);
       }
     }
-    useEffect(() => {
-        listCribroom();
-      }, []);
-
-    const listCribroom = async () => {
-        try {
-          const response = await getAllCribrooms();
-          setCribrooms(response.data);
-        } catch (error) {
-          console.log('Error fetching SalasCunas:', error);
-        }
-      };
-      
-    const listShift = async () => {
-        try {
-          const response = await getAllShifts();
-          setShiftOptions(response.data);
-        } catch (error) {
-          console.log('Error fetching Shifts:', error);
-        }
-      };
-
-    const listZone = async () => {
-        try {
-          const response = await getAllZones();
-          setZoneOptions(response.data);
-        } catch (error) {
-          console.log('Error fetching Zones:', error);
-        }
-    };
-
-    const handleEdit = async (event) => {
-        event.preventDefault()
-        const formData = new FormData(event.target);
-        const payload = {
-            name: formData.get("nameCR"),
-            code: formData.get("codeCR"),
-            max_capacity: formData.get("max_capacityCR"),
-            street: formData.get("streetCR"),
-            house_number: formData.get("house_numberCR"),
-            shift: formData.get("shiftCR"),
-            zone: formData.get("zoneCR"),
-        };
-
-        if(selectedCribroom){
-            try {
-                let response = await fetch('http://127.0.0.1:8000/api/cribroom/'+ selectedCribroom +'/', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(payload),
-                });
-    
-                if (response.ok){
-                    console.log('Cribroom Updated')
-                } else {
-                    console.log('Failed to Update')
-                }
-    
-            } catch (err) {
-                alert(err)
-            }
-        }
-    }
-  };
 
     const handleDelete = async (event) => {
       event.preventDefault()
@@ -206,4 +135,3 @@ export default function CribroomDashboard() {
       </div>
     </body>
   );
-}
