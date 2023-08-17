c8711ea3d55dcaed06757f2e213f1c4a95674378
Franco Ball <francoball181@gmail.com>
2023-08-17 15:49:34 -0300

    Changed login redirect

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -59,7 +59,7 @@ export const AuthProvider = ({children}) => {
             setAuthTokens(data)
             setUser(jwt_decode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
-            history('/crear-sala')
+            history('/gestion-sala')
         } else {
             alert('Something went wrong')
         }
