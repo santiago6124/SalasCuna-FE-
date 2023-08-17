778b614415493c8e6c7002065030e7e6a6e17713
Lichi <lichimarquez04@gmail.com>
2023-08-17 11:46:11 -0300

    cribroom without depth in dashboard

--- frontend/src/api/salasCuna.api.js
+++ frontend/src/api/salasCuna.api.js
@@ -1,13 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getAllCribrooms = () => {
-  return axios.get('http://127.0.0.1:8000/api/cribroom/');
+  return axios.get("http://127.0.0.1:8000/api/cribroom/");
+};
+
+export const getAllCribroomsWithoutDepth = () => {
+  return axios.get("http://127.0.0.1:8000/api/cribroom/?no_depth");
 };
 
 export const getAllZones = () => {
-  return axios.get('http://127.0.0.1:8000/api/zone/');
+  return axios.get("http://127.0.0.1:8000/api/zone/");
 };
 
 export const getAllShifts = async () => {
-  return axios.get('http://127.0.0.1:8000/api/shift/');
+  return axios.get("http://127.0.0.1:8000/api/shift/");
 };
