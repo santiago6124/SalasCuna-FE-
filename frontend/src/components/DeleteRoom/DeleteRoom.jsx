5ed4d9b80b7b10240c7a31282d9cb173c167dbc7
JoseUrtubey <joseaurtubey@gmail.com>
2023-08-17 11:51:33 -0300

    DeleteRoom Modal Created ~_~

--- frontend/src/components/DeleteRoom/DeleteRoom.jsx
+++ frontend/src/components/DeleteRoom/DeleteRoom.jsx
@@ -3,34 +3,40 @@ import Col from "react-bootstrap/Col/";
 import Row from "react-bootstrap/Row/";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
+import Modal from "react-bootstrap/Modal";
 
 import React from "react";
 
-export function DeleteRoom() {
-    return (
-        <Form className="conteiner-form-eliminar">
-            <h1 className="titulo-eliminar">Eliminar Sala Cuna</h1>
-            <div className="contenedor-linea-eliminar">
-                <hr className="linea-eliminar"></hr>
+export function DeleteRoom(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Form className="conteiner-form-eliminar">
+        <h1 className="titulo-eliminar">Eliminar Sala Cuna</h1>
+        <div className="contenedor-linea-eliminar">
+          <hr className="linea-eliminar"></hr>
+        </div>
+        <Row>
+          <Col xs={9} className="conteiner-eliminar"></Col>
+          <Form.Group className="mb-3">
+            <Form.Label className="mb-1 mt-3">
+              Esta Seguro que desea Deshabilitar la Sala Cuna
+            </Form.Label>
+            <div className="contenedor-boton-eliminar mt-4">
+              <Button className="boton-eliminar" boton variant="danger">
+                Dehabilitar
+              </Button>
+              <Button className="boton-edit mt-3" boton variant="primary">
+                Cancelar
+              </Button>
             </div>
-            <Row>
-                <Col xs={9} className="conteiner-eliminar"></Col>
-                <Form.Group className="mb-3">
-                    <Form.Label className="mb-1 mt-3">
-                        Seleccionar Sala Cuna Que Desea Eliminar
-                    </Form.Label>
-                    <Form.Select placeholder="Seleccionar Sala Cuna" className="mb-1">
-                        <option value="Sala Cuna 1">Sala Cuna 1</option>
-                        <option value="Sala Cuna 2">Sala Cuna 2</option>
-                        <option value="Sala Cuna 3">Sala Cuna 3</option>
-                    </Form.Select>
-                    <div className="contenedor-boton-eliminar mt-4">
-                        <Button className="boton-eliminar" boton variant="primary">
-                            Eliminar
-                        </Button>
-                    </div>
-                </Form.Group>
-            </Row>
-        </Form>
-    );
+          </Form.Group>
+        </Row>
+      </Form>
+    </Modal>
+  );
 }
