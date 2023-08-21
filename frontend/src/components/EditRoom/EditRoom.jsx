bfc07a74d3b0b9dd06a0b4280f30f81fc7355f8d
Lichi <lichimarquez04@gmail.com>
2023-08-21 17:23:01 -0300

    default dropdowns values (broke submit value dd)

--- frontend/src/components/EditRoom/EditRoom.jsx
+++ frontend/src/components/EditRoom/EditRoom.jsx
@@ -10,19 +10,21 @@ import { Button } from "react-bootstrap";
 
 import React, { useState, useEffect } from "react";
 
-import { getAllShifts, getAllZones } from "../../api/salasCuna.api";
+import { getAllShifts, getAllZones, getAllCribroomsWithoutDepth } from "../../api/salasCuna.api";
 
 export function UpdateRoom(props) {
   const [zoneOptions, setZoneOptions] = useState([]);
   const [shiftOptions, setShiftOptions] = useState([]);
   const [selectedCribroom, setSelectedCribroom] = useState("");
-  const [selectedZona, setSelectedZone] = useState("");
+  const [selectedZone, setSelectedZone] = useState("");
   const [selectedShift, setSelectedShift] = useState("");
+  const [cribroom, setCribroom] = useState([]);
 
   useEffect(() => {
     loadZones();
     loadShifts();
     setSelectedCribroom(props.id);
+    loadSelectedCribroom(props.id); // Load selected cribroom data
   }, []);
 
   const loadZones = async () => {
@@ -43,6 +45,24 @@ export function UpdateRoom(props) {
     }
   };
 
+  async function loadSelectedCribroom(cribroomId) {
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/cribroom/?no_depth&id=" + cribroomId,
+        { method: "GET" }
+      );
+      if (response.ok) {
+        const data = await response.json(); // Extract JSON data
+        const crib = data[0];
+        setCribroom(crib);
+      } else {
+        console.error("Error fetching selected cribroom data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching selected cribroom data:", error);
+    }
+  }
+
   const handleEdit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -50,10 +70,10 @@ export function UpdateRoom(props) {
       name: formData.get("nameCR"),
       code: formData.get("codeCR"),
       max_capacity: formData.get("max_capacityCR"),
-      street: formData.get("streetCR"),
+      street: formData.get("streetCR") ? formData.get("streetCR"): "", // if null, empty string in order to not broke xd
       house_number: formData.get("house_numberCR"),
       shift: formData.get("shiftCR"),
-      zone: formData.get("zoneCR")
+      zone: formData.get("zoneCR"),
     };
     if (selectedCribroom) {
       try {
@@ -79,13 +99,13 @@ export function UpdateRoom(props) {
     }
   };
 
-  const handleShiftChange = (event) => {
+  function handleShiftChange(event) {
     setSelectedShift(event.target.value);
-  };
+  }
 
-  const handleZoneChange = (event) => {
+  function handleZoneChange(event) {
     setSelectedZone(event.target.value);
-  };
+  }
 
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
@@ -103,7 +123,7 @@ export function UpdateRoom(props) {
                 type="text"
                 placeholder="Editar El Nombre De La Sala"
                 name="nameCR"
-                defaultValue={props.id}
+                defaultValue={cribroom ? cribroom.name : ""}
               />
             </Form.Group>
             <Form.Group className="mb-3">
@@ -112,6 +132,7 @@ export function UpdateRoom(props) {
                 type="text"
                 placeholder="Editar El Nombre De La Sala"
                 name="codeCR"
+                defaultValue={cribroom ? cribroom.code : ""}
               />
             </Form.Group>
             <Form.Group className="mb-3">
@@ -120,6 +141,7 @@ export function UpdateRoom(props) {
                 type="number"
                 placeholder="Editar La Capacidad Maxima De La Sala"
                 name="max_capacityCR"
+                defaultValue={cribroom ? cribroom.max_capacity : ""}
               />
             </Form.Group>
             <Row className="mb-1">
@@ -129,6 +151,7 @@ export function UpdateRoom(props) {
                   type="text"
                   placeholder="Editar Calle"
                   name="streetCR"
+                  defaultValue={cribroom ? cribroom.street : ""}
                 />
               </Col>
               <Col>
@@ -137,6 +160,7 @@ export function UpdateRoom(props) {
                   type="number"
                   placeholder="Nro"
                   name="house_numberCR"
+                  defaultValue={cribroom ? cribroom.house_number : ""}
                 />
               </Col>
             </Row>
@@ -149,7 +173,7 @@ export function UpdateRoom(props) {
                     <Form.Select
                       name="shiftCR"
                       as="select"
-                      value={selectedShift}
+                      value={selectedShift ? selectedShift: cribroom.shift}
                       className="mb-1"
                       onChange={handleShiftChange}
                     >
@@ -170,7 +194,7 @@ export function UpdateRoom(props) {
                 <Form.Select
                   name="zoneCR"
                   as="select"
-                  value={selectedZona}
+                  value={selectedZone ? selectedZone: cribroom.zone}
                   onChange={handleZoneChange}
                 >
                   <option value="" disabled>
