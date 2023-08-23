34543058997d4a9bfde1e3b5488de907a9d7ff78
JoseUrtubey <joseaurtubey@gmail.com>
2023-08-23 11:49:50 -0300

    Created form on modal

--- frontend/src/components/UserManagement/EditUserModal.jsx
+++ frontend/src/components/UserManagement/EditUserModal.jsx
@@ -12,153 +12,147 @@ import React, { useState, useEffect } from "react";
 import { getAllRoles } from "../../api/salasCuna.api";
 
 export function UpdateUser(props) {
-    const [selectedUser, setSelectedUser] = useState([]);
-    const [roleOptions, setRolesOptions] = useState([]);
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [roleOptions, setRolesOptions] = useState([]);
 
+  useEffect(() => {
+    loadRoles();
+    loadSelectedUser(props.id);
+  }, []);
 
-    useEffect(() => {
-        loadRoles();
-        loadSelectedUser(props.id)
-    }, []);
-
-    async function loadSelectedUser(user_id) {
-        try {
-            const responseUsers = axios.get("/api/user/" + user_id);
-            if (response.ok) {
-                const userData = await responseUsers.data;
-                setSelectedUser(userData[0])
-            } else {
-                console.error("Error fetching selected user data:", response.statusText);
-            }
-
-        } catch (error) {
-            console.error("Error fetching selected user data:", error);
-        }
+  async function loadSelectedUser(user_id) {
+    try {
+      const responseUsers = axios.get("/api/user/" + user_id);
+      if (response.ok) {
+        const userData = await responseUsers.data;
+        setSelectedUser(userData[0]);
+      } else {
+        console.error(
+          "Error fetching selected user data:",
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching selected user data:", error);
     }
+  }
 
-    const loadRoles = async () => {
-        try {
-          const response = await getAllRoles();
-          setRolesOptions(response.data);
-        } catch (error) {
-          console.error("Error fetching role options:", error);
-        }
-      };
+  const loadRoles = async () => {
+    try {
+      const response = await getAllRoles();
+      setRolesOptions(response.data);
+    } catch (error) {
+      console.error("Error fetching role options:", error);
+    }
+  };
 
-    return (
-        <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
-          <div className="contenedor-form-wrapper">
-            <Container fluid className="conteiner-form-room">
-              <Form onSubmit={handleEdit} className="conteiner-form-edit">
-                <h1 className="titulo">Editar Usuario</h1>
-                <div className="contenedor-linea">
-                  <hr className="linea"></hr>
-                </div>
-    
-                <Form.Group className="mb-3">
-                  <Form.Label className="mb-1">Nombre Del Usuario</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Editar El Nombre Del Usuario"
-                    name="first_name"
-                    defaultValue={user ? user.first_name : ""}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label className="mb-1">Codigo De Sala</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Editar El Nombre De La Sala"
-                    name="codeCR"
-                    defaultValue={cribroom ? cribroom.code : ""}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label className="mb-1">Capacidad Maxima</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder="Editar La Capacidad Maxima De La Sala"
-                    name="max_capacityCR"
-                    defaultValue={cribroom ? cribroom.max_capacity : ""}
-                  />
-                </Form.Group>
-                <Row className="mb-1">
-                  <Col xs={9}>
-                    <Form.Label className="mb-1">Calle</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Editar Calle"
-                      name="streetCR"
-                      defaultValue={cribroom ? cribroom.street : ""}
-                    />
-                  </Col>
-                  <Col>
-                    <Form.Label className="mb-1">Nro</Form.Label>
-                    <Form.Control
-                      type="number"
-                      placeholder="Nro"
-                      name="house_numberCR"
-                      defaultValue={cribroom ? cribroom.house_number : ""}
-                    />
-                  </Col>
-                </Row>
-    
-                <Row className="mb-3">
-                  <Col>
-                    <Form.Group>
-                      <Form.Label className="mb-1">Turno</Form.Label>
-                      <Form.Group className="mb-3">
-                        <Form.Select
-                          name="shiftCR"
-                          as="select"
-                          value={selectedShift ? selectedShift: cribroom.shift}
-                          className="mb-1"
-                          onChange={handleShiftChange}
-                        >
-                          <option value="" disabled>
-                            Seleccionar Turno
-                          </option>
-                          {shiftOptions.map((shift) => (
-                            <option key={shift.id} value={shift.id}>
-                              {shift.name}
-                            </option>
-                          ))}
-                        </Form.Select>
-                      </Form.Group>
-                    </Form.Group>
-                  </Col>
-                  <Col>
-                    <Form.Label className="mb-1">Zona</Form.Label>
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
+      <div className="contenedor-form-wrapper">
+        <Container fluid className="conteiner-form-room">
+          <Form onSubmit={handleEdit} className="conteiner-form-edit">
+            <h1 className="titulo">Editar Usuario</h1>
+            <div className="contenedor-linea">
+              <hr className="linea"></hr>
+            </div>
+
+            <Form.Group className="mb-3">
+              <Form.Label className="mb-1">Nombre Del Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Editar El Nombre Del Usuario"
+                name="first_name"
+                defaultValue={user ? user.first_name : ""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="mb-1">Apellido del usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Editar el apellido del usuario"
+                name="last_name"
+                defaultValue={user ? user.last_name : ""}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="mb-1">Direccion Del Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Editar la direccion del usuario"
+                name="address"
+                defaultValue={user ? user.address : ""}
+              />
+              <Form.Label className="mb-1">Departamento Del Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Editar el departamento del usuario"
+                name="department"
+                defaultValue={user ? user.department : ""}
+              />
+              <Form.Label className="mb-1">Ciudad Del Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Editar la ciudad del usuario"
+                name="city"
+                defaultValue={user ? user.city : ""}
+              />
+            </Form.Group>
+            <Row className="mb-1">
+              <Col xs={9}>
+                <Form.Label className="mb-1">DNI del usuario</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Editar DNI del usuario"
+                  name="dni"
+                  defaultValue={user ? user.dni : ""}
+                />
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label className="mb-1">Rol del usuario</Form.Label>
+                  <Form.Group className="mb-3">
                     <Form.Select
-                      name="zoneCR"
+                      name="role"
                       as="select"
-                      value={selectedZone ? selectedZone: cribroom.zone}
-                      onChange={handleZoneChange}
+                      value={roleOptions ? roleOptions : user.role}
+                      className="mb-1"
+                      /* onChange={} */
                     >
                       <option value="" disabled>
-                        Seleccionar Zona
+                        Seleccionar Turno
                       </option>
-                      {zoneOptions.map((zone) => (
-                        <option key={zone.id} value={zone.id}>
-                          {zone.name}
+                      {roleOptions.map((role) => (
+                        <option key={role.id} value={role.id}>
+                          {role.name}
                         </option>
                       ))}
                     </Form.Select>
-                  </Col>
-                </Row>
-                <div className="contenedor-boton-qr ">
-                  <Button
-                    className="boton-edit mt-3"
-                    boton
-                    variant="primary"
-                    type="submit"
-                  >
-                    Editar Sala Cuna
-                  </Button>
-                </div>
-              </Form>
-            </Container>
-          </div>
-        </Modal>
-    );
+                  </Form.Group>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group className="mb-3">
+              <Form.Label className="mb-1">Numero de telefono</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Editar el numero de telefono del usuario"
+                name="last_name"
+                defaultValue={user ? user.phone_number : ""}
+              />
+            </Form.Group>
+            <div className="contenedor-boton-qr ">
+              <Button
+                className="boton-edit mt-3"
+                boton
+                variant="primary"
+                type="submit"
+              >
+                Editar Sala Cuna
+              </Button>
+            </div>
+          </Form>
+        </Container>
+      </div>
+    </Modal>
+  );
 }
