458f0bb677c81f4e85ee43ab939ec58a77389072
Franco-Ball <francoball181@gmail.com>
2023-08-23 10:12:18 -0300

    created files for UserEdit modals

---
+++ frontend/src/components/UserManagement/EditUserModal.jsx
@@ -0,0 +1,11 @@
+import "../UserManagement/EditUserModal.css";
+
+import Modal from "react-bootstrap/Modal";
+
+import Col from "react-bootstrap/Col/";
+import Row from "react-bootstrap/Row/";
+import Form from "react-bootstrap/Form/";
+import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+import React, { useState, useEffect } from "react";
