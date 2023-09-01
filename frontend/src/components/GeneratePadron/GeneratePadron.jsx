e4804b749fe886816b3348252f6c5864b8cec72d
jose urtubey <joseaurtubey@gmail.com>
2023-09-01 09:44:21 -0300

    Generate Padron styled

--- frontend/src/components/GeneratePadron/GeneratePadron.jsx
+++ frontend/src/components/GeneratePadron/GeneratePadron.jsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Col from 'react-bootstrap/Col/';
-import Row from 'react-bootstrap/Row/';
-import Form from 'react-bootstrap/Form/';
-import axios from 'axios';
-import { DataGrid } from '@mui/x-data-grid';
-import Button from 'react-bootstrap/Button/';
-
+import React, { useState, useEffect } from "react";
+import Col from "react-bootstrap/Col/";
+import Row from "react-bootstrap/Row/";
+import Form from "react-bootstrap/Form/";
+import axios from "axios";
+import { DataGrid } from "@mui/x-data-grid";
+import Button from "react-bootstrap/Button/";
+import Menu from "../Menu/Menu";
 
 const GeneratePadron = () => {
-
   const [zoneOptions, setZoneOptions] = useState([]);
   const [selectedZone, setSelectedZone] = useState("");
   const [cribrooms, setCribrooms] = useState([]);
   const handlePdfClick = () => {
-      // Implement your PDF generation logic here
-      console.log('Generate PDF logic will be implemented here');
+    // Implement your PDF generation logic here
+    console.log("Generate PDF logic will be implemented here");
   };
   const handleExcelClick = () => {
     // Implement your Excel generation logic here
-    console.log('Generate Excel logic will be implemented here');
+    console.log("Generate Excel logic will be implemented here");
   };
   useEffect(() => {
     loadZones();
@@ -32,7 +31,9 @@ const GeneratePadron = () => {
 
   const loadCribrooms = async (zoneId) => {
     try {
-      const response = await axios.get(`http://127.0.0.1:8000/api/cribroom/?zone=${zoneId}`);
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/cribroom/?zone=${zoneId}`
+      );
       const jsonData = response.data;
       setCribrooms(jsonData);
     } catch (error) {
@@ -55,69 +56,84 @@ const GeneratePadron = () => {
   };
 
   const columns = [
-      { field: 'code', headerName: 'Codigo' },
-      { field: 'name', headerName: 'Nombre Sala' },
-      { field: 'cuit', headerName: 'CUIT' },
-      { field: 'entidad', headerName: 'Entidad' },
-      { field: 'cantidad_ninos', headerName: 'Cantidad de Niños' },
-      {
-          field: 'select',
-          headerName: 'Seleccionar',
-          sortable: false,
-          filterable: false,
-          width: 120,
-          renderCell: (params) => (
-              <input type="checkbox" checked={params.row.selected} />
-          ),
-      },
+    { field: "code", headerName: "Codigo" },
+    { field: "name", headerName: "Nombre Sala" },
+    { field: "cuit", headerName: "CUIT" },
+    { field: "entidad", headerName: "Entidad" },
+    { field: "cantidad_ninos", headerName: "Cantidad de Niños" },
+    {
+      field: "select",
+      headerName: "Seleccionar",
+      sortable: false,
+      filterable: false,
+      width: 120,
+      renderCell: (params) => (
+        <input type="checkbox" checked={params.row.selected} />
+      ),
+    },
   ];
-  
 
   return (
-      <div className='container-report mt-5 mx-auto'>
-          <div className='container-titulo-report '>
-              <h1> Generar Padron </h1>
-          </div>
-          <div className='contenedor-linea-report'>
-              <hr className='linea-report'></hr>
-          </div>
-          <Row className="mb-3">
-              <Col>
-                  <div className='input-select'>
-                      <Form.Select
-                          name="zoneCR"
-                          as="select"
-                          value={selectedZone}
-                          onChange={handleSelectChange}
-                      >
-                          <option value="" disabled>
-                              Seleccionar Zona
-                          </option>
-                          {zoneOptions.map((zone) => (
-                              <option key={zone.id} value={zone.id}>
-                                  {zone.name}
-                              </option>
-                          ))}
-                      </Form.Select>
-                  </div>
-              </Col>
-              <Col>
-              <Button variant="primary" onClick={handlePdfClick} className="mb-3">
-PDF
-</Button>
-<Button variant="success" onClick={handleExcelClick} className="mb-3 ms-2">
-            EXCEL
-          </Button>
-              </Col>
-          </Row>
-
-          <div>
-              <div style={{ height: 400, width: '100%' }}>
-                  <DataGrid rows={cribrooms} columns={columns} columnBuffer={2} columnThreshold={2} />
-              </div>
-          </div>
-      </div>
+    <>
+      <body>
+        <header className="header">
+          <Menu />
+        </header>
+        <h1 className="titulo-cb">Generar Padron</h1>
+        <div className="contenedor-linea-report">
+          <hr className="linea-report"></hr>
+        </div>
+        <Row>
+          <Form.Label className="mb-1 ms-3">Seleccionar Zona</Form.Label>
+          <Col className="col-md-3 add-payout-button ">
+            <Form.Select
+              className="mb-1"
+              name="zoneCR"
+              as="select"
+              value={selectedZone}
+              onChange={handleSelectChange}
+            >
+              <option value="" disabled>
+                Seleccionar Zona
+              </option>
+              {zoneOptions.map((zone) => (
+                <option key={zone.id} value={zone.id}>
+                  {zone.name}
+                </option>
+              ))}
+            </Form.Select>
+          </Col>
+          <Col className="col-sm-1">
+            <Button
+              variant="primary"
+              onClick={handlePdfClick}
+              className="mt-3 ms-2 add-payout-button"
+            >
+              PDF
+            </Button>
+          </Col>
+          <Col className="col-sm-1">
+            <Button
+              variant="success"
+              onClick={handleExcelClick}
+              className="mt-3 ms-2 add-payout-button"
+            >
+              EXCEL
+            </Button>
+          </Col>
+        </Row>
+        <div className="DataGrid-Wrapper-report">
+          <DataGrid
+            style={{ borderRadius: "15px", margin: "20px" }}
+            rows={cribrooms}
+            columns={columns}
+            columnBuffer={2}
+            columnThreshold={2}
+          />
+        </div>
+      </body>
+    </>
   );
-}
+};
 
 export default GeneratePadron;
