97d9a183a808831ad4207b5bebec86795bd2811c
facuolivamar <facundoolivam@gmail.com>
2023-09-10 11:55:55 -0300

    try catch for requests block

--- frontend/src/components/FilesToDb/FilesToDb.jsx
+++ frontend/src/components/FilesToDb/FilesToDb.jsx
@@ -30,153 +30,176 @@ export function FilesToDb() {
       var parsedDataKeys = Object.keys(parsedData[6]);
       // console.log(parsedDataKeys);
 
-      var locality = {
-        "locality": parsedData[8][parsedDataKeys[11]]
+      try {
+        var locality = {
+          "locality": parsedData[8][parsedDataKeys[11]]
+        }
+        var localityURL = `/api/LocalityListView/?locality=${locality.locality}`;
+
+        var localityResponse = await axios.get(localityURL);
+        if (localityResponse.data.length === 0) {
+          var localityResponse = await axios.post('/api/LocalityListCreateView/', locality);
+          localityResponse = localityResponse.data['id'];
+        } else {
+          console.log("The array is not empty. (localityResponse)");
+          localityResponse = localityResponse.data[0]['id'];
+        }
+      } catch (error) {
+        console.error("An error occurred (locality request):", error);
       }
-      var localityURL = `/api/LocalityListView/?locality=${locality.locality}`;
 
-      var localityResponse = await axios.get(localityURL);
-      if (localityResponse.data.length === 0) {
-        var localityResponse = await axios.post('/api/LocalityListCreateView/', locality);
-        localityResponse = localityResponse.data['id'];
-      } else {
-        console.log("The array is not empty. (localityResponse)");
-        localityResponse = localityResponse.data[0]['id'];
+      try {
+        var cribroom = {
+          "name": parsedData[0]['Sala Cuna:'],
+          "entity": parsedData[4]['Sala Cuna:'],
+          "CUIT": 12,
+          "street": 'NO ESPECIFICADO',
+          "code": 12321,
+          "max_capacity": 0,
+          "is_active": true,
+          "house_number": 0,
+          "department": 7,
+          "neighborhood": 7,
+          "shift": 7,
+          "zone": 7,
+          "locality": localityResponse
+        };
+        var cribroomResponse = await axios.post('/api/cribroom/', cribroom);
+        cribroomResponse = cribroomResponse.data['id'];
+
+        cribroom['code'] = files[i].name.split('-')[1];
+      } catch (error) {
+        console.error("An error occurred (cribroom request):", error);
       }
-
-      var cribroom = {
-        "name": parsedData[0]['Sala Cuna:'],
-        "entity": parsedData[4]['Sala Cuna:'],
-        "CUIT": 12,
-        "street": 'NO ESPECIFICADO',
-        "code": 12321,
-        "max_capacity": 0,
-        "is_active": true,
-        "house_number": 0,
-        "department": 7,
-        "neighborhood": 7,
-        "shift": 7,
-        "zone": 7,
-        "locality": localityResponse
-      };
-      var cribroomResponse = await axios.post('/api/cribroom/', cribroom);
-      cribroomResponse = cribroomResponse.data['id'];
-
-      cribroom['code'] = files[i].name.split('-')[1];
-
-      console.log('before for each');
 
       for (let rowIndex = 7; rowIndex < parsedData.length; rowIndex++) {
           if (parsedData[rowIndex][parsedDataKeys[1]] === cribroom.code && typeof parsedData[rowIndex][parsedDataKeys[0]] === 'number' ) {
 
-          var phoneFeature = {
-            "feature": parsedData[rowIndex][parsedDataKeys[12]]
-          }
-          var phoneFeatureURL = `/api/PhoneFeatureListCreateView/?feature=${phoneFeature.feature}`;
+          try {
+            var phoneFeature = {
+              "feature": parsedData[rowIndex][parsedDataKeys[12]]
+            }
+            var phoneFeatureURL = `/api/PhoneFeatureListCreateView/?feature=${phoneFeature.feature}`;
 
-          // var phoneFeatureResponse = {'id':0};
-          var phoneFeatureResponse = await axios.get(phoneFeatureURL);
-          if (phoneFeatureResponse.data.length === 0) {
-            var phoneFeatureResponse = await axios.post('/api/PhoneFeatureListCreateView/', phoneFeature);
-            phoneFeatureResponse = phoneFeatureResponse.data['id']
-          } else {
-            phoneFeatureResponse = phoneFeatureResponse.data[0]['id'];
-          }
-
-          console.log('phoneFeatureResponse:' , phoneFeatureResponse);
-
-          var guardian = {
-                "first_name": parsedData[rowIndex][parsedDataKeys[14]],
-                "last_name": parsedData[rowIndex][parsedDataKeys[14]],
-                "dni": parsedData[rowIndex][parsedDataKeys[15]],
-                "phone_number": parsedData[rowIndex][parsedDataKeys[13]].toString().split('/')[0],
-                "phone_Feature": phoneFeatureResponse,
-                'guardian_Type' : 1,
-                'gender' : 1,
-                // "guardian_Type": {
-                //     "id": 2,
-                //     "type": "Guardian"
-                // },
-                // "gender": {
-                //     "id": 2,
-                //     "gender": "Female"
-                // }
-          }
-          console.log('guardian: ', guardian);
-          var guardianResponse = await axios.post('/api/GuardianListCreateView/', guardian);
-          guardianResponse = guardianResponse.data['id'];
-
-          var neighborhood_or_department = {
-              "neighborhood": parsedData[rowIndex][parsedDataKeys[10]]
-          }
-          var neighborhood_or_departmentURL = `/api/NeighborhoodListCreateView/?neighborhood=${neighborhood_or_department.neighborhood}`;
-
-          var neighborhood_or_departmentResponse = await axios.get(neighborhood_or_departmentURL);
-          if (neighborhood_or_departmentResponse.data.length === 0) {
-            var neighborhood_or_departmentResponse = await axios.post('/api/NeighborhoodListCreateView/', neighborhood_or_department);
-            neighborhood_or_departmentResponse = neighborhood_or_departmentResponse.data['id']
-          } else {
-            neighborhood_or_departmentResponse = neighborhood_or_departmentResponse.data[0]['id'];
+            // var phoneFeatureResponse = {'id':0};
+            var phoneFeatureResponse = await axios.get(phoneFeatureURL);
+            if (phoneFeatureResponse.data.length === 0) {
+              var phoneFeatureResponse = await axios.post('/api/PhoneFeatureListCreateView/', phoneFeature);
+              phoneFeatureResponse = phoneFeatureResponse.data['id']
+            } else {
+              phoneFeatureResponse = phoneFeatureResponse.data[0]['id'];
+            }
+          } catch (error) {
+            console.error("An error occurred (phoneFeature request):", error);
           }
 
-          var gender = {
-            "gender": parsedData[rowIndex][parsedDataKeys[7]]
+          try {
+            var guardian = {
+                  "first_name": parsedData[rowIndex][parsedDataKeys[14]],
+                  "last_name": parsedData[rowIndex][parsedDataKeys[14]],
+                  "dni": parsedData[rowIndex][parsedDataKeys[15]],
+                  "phone_number": parsedData[rowIndex][parsedDataKeys[13]].toString().split('/')[0],
+                  "phone_Feature": phoneFeatureResponse,
+                  'guardian_Type' : 1,
+                  'gender' : 1,
+                  // "guardian_Type": {
+                  //     "id": 2,
+                  //     "type": "Guardian"
+                  // },
+                  // "gender": {
+                  //     "id": 2,
+                  //     "gender": "Female"
+                  // }
+            }
+            var guardianResponse = await axios.post('/api/GuardianListCreateView/', guardian);
+            guardianResponse = guardianResponse.data['id'];
+          } catch (error) {
+            console.error("An error occurred (guardian request):", error);
           }
-          var genderURL = `/api/GenderListCreateView/?gender=${gender.gender}`;
 
-          var genderResponse = await axios.get(genderURL);
-          if (genderResponse.data.length === 0) {
-            var genderResponse = await axios.post('/api/GenderListCreateView/', gender);
-            genderResponse = genderResponse.data['id']
-          } else {
-            genderResponse = genderResponse.data[0]['id'];
+          try {
+            var neighborhood_or_department = {
+                "neighborhood": parsedData[rowIndex][parsedDataKeys[10]]
+            }
+            var neighborhood_or_departmentURL = `/api/NeighborhoodListCreateView/?neighborhood=${neighborhood_or_department.neighborhood}`;
+
+            var neighborhood_or_departmentResponse = await axios.get(neighborhood_or_departmentURL);
+            if (neighborhood_or_departmentResponse.data.length === 0) {
+              var neighborhood_or_departmentResponse = await axios.post('/api/NeighborhoodListCreateView/', neighborhood_or_department);
+              neighborhood_or_departmentResponse = neighborhood_or_departmentResponse.data['id']
+            } else {
+              neighborhood_or_departmentResponse = neighborhood_or_departmentResponse.data[0]['id'];
+            }
+          } catch (error) {
+            console.error("An error occurred (neighborhood_or_department request):", error);
           }
 
-          var shift = {
-            "name": parsedData[rowIndex][parsedDataKeys[16]]
-          }
-          var shiftURL = `/api/ShiftListCreateView/?shift=${shift.name}`;
+          try {
+            var gender = {
+              "gender": parsedData[rowIndex][parsedDataKeys[7]]
+            }
+            var genderURL = `/api/GenderListCreateView/?gender=${gender.gender}`;
 
-          var shiftResponse = await axios.get(shiftURL);
-          if (shiftResponse.data.length === 0) {
-            var shiftResponse = await axios.post('/api/ShiftListCreateView/', shift);
-            shiftResponse = shiftResponse.data['id']
-          } else {
-            shiftResponse = shiftResponse.data[0]['id'];
+            var genderResponse = await axios.get(genderURL);
+            if (genderResponse.data.length === 0) {
+              var genderResponse = await axios.post('/api/GenderListCreateView/', gender);
+              genderResponse = genderResponse.data['id']
+            } else {
+              genderResponse = genderResponse.data[0]['id'];
+            }
+          } catch (error) {
+            console.error("An error occurred (gender request):", error);
           }
-          console.log('shiftResponse: ', shiftResponse);
-          console.log('shift: ', shift);
 
-          var child = {
-              "guardian": guardianResponse,
-              "first_name": parsedData[rowIndex][parsedDataKeys[3]],
-              "last_name": parsedData[rowIndex][parsedDataKeys[2]],
-              "dni": parsedData[rowIndex][parsedDataKeys[4]],
-              // "birthdate": parsedData[rowIndex][parsedDataKeys[5]],
-              "birthdate": '1913-06-15',
-              "street": parsedData[rowIndex][parsedDataKeys[8]],
-              "house_number": parsedData[rowIndex][parsedDataKeys[9]],
-              "registration_date": "2022-07-23",
-              // "disenroll_date": "2023-08-09",
-              "is_active": parsedData[rowIndex][parsedDataKeys[17]] == 'BAJA' ? false : true,
-              "locality": localityResponse,
-              "neighborhood": neighborhood_or_departmentResponse,
-              "gender": genderResponse,
-              "cribroom": cribroomResponse,
-              "shift": shiftResponse,
+          try {
+            var shift = {
+              "name": parsedData[rowIndex][parsedDataKeys[16]]
+            }
+            var shiftURL = `/api/ShiftListCreateView/?name=${shift.name}`;
+
+            var shiftResponse = await axios.get(shiftURL);
+            if (shiftResponse.data.length === 0) {
+              var shiftResponse = await axios.post('/api/ShiftListCreateView/', shift);
+              shiftResponse = shiftResponse.data['id']
+            } else {
+              shiftResponse = shiftResponse.data[0]['id'];
+            }
+          } catch (error) {
+            console.error("An error occurred (shift request):", error);
           }
-          console.log('child: ', child);
-          var childResponse = await axios.post('/api/ChildListCreateView/', child);
-          console.log('childResponse: ', childResponse);
 
+          try {
+            var child = {
+                "guardian": guardianResponse,
+                "first_name": parsedData[rowIndex][parsedDataKeys[3]],
+                "last_name": parsedData[rowIndex][parsedDataKeys[2]],
+                "dni": parsedData[rowIndex][parsedDataKeys[4]],
+                // "birthdate": parsedData[rowIndex][parsedDataKeys[5]],
+                "birthdate": '1913-06-15',
+                "street": parsedData[rowIndex][parsedDataKeys[8]],
+                "house_number": parsedData[rowIndex][parsedDataKeys[9]],
+                "registration_date": "2022-07-23",
+                // "disenroll_date": "2023-08-09",
+                "is_active": parsedData[rowIndex][parsedDataKeys[17]] == 'BAJA' ? false : true,
+                "locality": localityResponse,
+                "neighborhood": neighborhood_or_departmentResponse,
+                "gender": genderResponse,
+                "cribroom": cribroomResponse,
+                "shift": shiftResponse,
+            }
+            console.log('child: ', child);
+            var childResponse = await axios.post('/api/ChildListCreateView/', child);
+          } catch (error) {
+            console.error("An error occurred (child request):", error);
+          }
         }
       }
 
-      if (allData.length === files.length) {
-        // Combine data from all files
-        const combinedData = allData.flat();
-        setData(combinedData);
-      }
+      // if (allData.length === files.length) {
+      //   // Combine data from all files
+      //   const combinedData = allData.flat();
+      //   setData(combinedData);
+      // }
     }
   }
 
