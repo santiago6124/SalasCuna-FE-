8a9af775b8fc563c5cf5b1aa1a903c85ed5cc6c1
FranciscoBrezzo <fran.brezzo@gmail.com>
2023-09-10 18:27:56 -0300

    redirects sidebar

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import '../Profile/Profile.css'
 
 
 import Button from 'react-bootstrap/Button'; // Importa Button de react-bootstrap
@@ -7,21 +8,10 @@ import Modal from 'react-bootstrap/Modal'; // Importa Modal de react-bootstrap
 
 export default function Profile({ show, handleClose }) {
     return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Titulo del Modal</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Contenido del modal rectangular.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Guardar cambios
-          </Button>
-        </Modal.Footer>
+      <Modal dialogClassName="card modal-90h" show={show} onHide={handleClose}>
+        
+          
+        
       </Modal>
     );
   }
