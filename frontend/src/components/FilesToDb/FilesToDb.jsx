2f93c2b061c9c7014df99aaef36eee950cadb4e9
facuolivamar <facundoolivam@gmail.com>
2023-09-10 03:57:48 -0300

    PhoneFeature requests

--- frontend/src/components/FilesToDb/FilesToDb.jsx
+++ frontend/src/components/FilesToDb/FilesToDb.jsx
@@ -28,7 +28,7 @@ export function FilesToDb() {
       allData.push(parsedData);
 
       var parsedDataKeys = Object.keys(parsedData[6]);
-      console.log(parsedDataKeys);
+      // console.log(parsedDataKeys);
 
       var locality = {
         "locality": parsedData[8][parsedDataKeys[11]]
@@ -39,7 +39,7 @@ export function FilesToDb() {
       if (localityResponse.data.length === 0) {
         var localityResponse = await axios.post('/api/LocalityListCreateView/', locality);
       } else {
-        console.log("The array is not empty.");
+        console.log("The array is not empty. (localityResponse)");
       }
 
       var cribroom = {
@@ -58,6 +58,91 @@ export function FilesToDb() {
         "locality": localityResponse.data['id']
       };
       var cribroomResponse = await axios.post('/api/cribroom/', cribroom);
+
+      cribroom['code'] = files[i].name.split('-')[1];
+
+      console.log('before for each');
+
+      for (let rowIndex = 7; rowIndex < parsedData.length; rowIndex++) {
+          if (parsedData[rowIndex][parsedDataKeys[1]] === cribroom.code && typeof parsedData[rowIndex][parsedDataKeys[0]] === 'number' ) {
+
+          var phoneFeature = {
+            "feature": parsedData[rowIndex][parsedDataKeys[12]]
+          }
+          var phoneFeatureURL = `/api/PhoneFeatureListCreateView/?feature=${phoneFeature.feature}`;
+
+          // var phoneFeatureResponse = {'id':0};
+          var phoneFeatureResponse = await axios.get(phoneFeatureURL);
+          if (phoneFeatureResponse.data.length === 0) {
+            var phoneFeatureResponse = await axios.post('/api/PhoneFeatureListCreateView/', phoneFeature);
+            console.log('phoneFeatureResponse:' , phoneFeatureResponse);
+          } else {
+            console.log("The array is not empty.");
+          }
+
+          var guardian = {
+                "first_name": parsedData[rowIndex][parsedDataKeys[14]],
+                "last_name": parsedData[rowIndex][parsedDataKeys[14]],
+                "dni": parsedData[rowIndex][parsedDataKeys[15]],
+                "phone_number": parsedData[rowIndex][parsedDataKeys[13]],
+                "phone_Feature": phoneFeatureResponse.data['id'],
+                // "guardian_Type": {
+                //     "id": 2,
+                //     "type": "Guardian"
+                // },
+                // "gender": {
+                //     "id": 2,
+                //     "gender": "Female"
+                // }
+          }
+          var guardianResponse = {'id':1};
+
+          var neighborhood_or_department = {
+              "neighborhood": parsedData[rowIndex][parsedDataKeys[10]]
+          }
+          var neighborhood_or_departmentResponse = {'id':1};
+
+          var gender = {
+            "gender": parsedData[rowIndex][parsedDataKeys[7]]
+          }
+          var genderResponse = {'id':1};
+
+          var shift = {
+            "name": parsedData[rowIndex][parsedDataKeys[16]]
+          }
+          var shiftResponse = {'id':1};
+
+          var child = {
+              "guardian": {
+                  "id": guardianResponse.id,
+              },
+              "first_name": parsedData[rowIndex][parsedDataKeys[3]],
+              "last_name": parsedData[rowIndex][parsedDataKeys[2]],
+              "dni": parsedData[rowIndex][parsedDataKeys[4]],
+              "birthdate": parsedData[rowIndex][parsedDataKeys[5]],
+              "street": parsedData[rowIndex][parsedDataKeys[8]],
+              "house_number": parsedData[rowIndex][parsedDataKeys[9]],
+              // "registration_date": "2022-07-23",
+              // "disenroll_date": "2023-08-09",
+              "is_active": parsedData[rowIndex][parsedDataKeys[17]] == 'BAJA' ? false : true,
+              "locality": {
+                  "id": localityResponse.id,
+              },
+              "neighborhood": {
+                  "id": neighborhood_or_departmentResponse.id,
+              },
+              "gender": {
+                  "id": genderResponse.id,
+              },
+              "cribroom": {
+                  "id": 0,
+              },
+              "shift": {
+                  "id": shiftResponse.id,
+              },
+          }
+        }
+      }
 
       if (allData.length === files.length) {
         // Combine data from all files
