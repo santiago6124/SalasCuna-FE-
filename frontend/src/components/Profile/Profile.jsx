a6bc55a8ef12bb7b18955abac2d540c3e4c68980
FranciscoBrezzo <fran.brezzo@gmail.com>
2023-09-12 09:04:36 -0300

    ...

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -8,9 +8,15 @@ import Modal from 'react-bootstrap/Modal'; // Importa Modal de react-bootstrap
 
 export default function Profile({ show, handleClose }) {
     return (
-      <Modal dialogClassName="card modal-90h" show={show} onHide={handleClose}>
+      <Modal  
+      dialogClassName="modal-90h custom-clip-path"  
+      show={show} 
+      onHide={handleClose} 
+      style={{ width: '250px', 
+               height: '500px'
+            }}>
         
-          
+    
         
       </Modal>
     );
