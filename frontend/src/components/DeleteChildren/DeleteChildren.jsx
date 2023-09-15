c64eb2924788d61826550b82f0928d0e07612e9c
jose urtubey <joseaurtubey@gmail.com>
2023-09-15 08:21:34 -0300

    modalized DeleteChildren

--- frontend/src/components/DeleteChildren/DeleteChildren.jsx
+++ frontend/src/components/DeleteChildren/DeleteChildren.jsx
@@ -1,28 +1,83 @@
-import React from 'react'
-import './DeleteChildren.css';
+import React from "react";
+import "./DeleteChildren.css";
 
-import {Button, Form} from 'react-bootstrap';
-
+import { Button, Form } from "react-bootstrap";
 
 export function DeleteChildren() {
-    return (
-        <Form className='conteiner-form-delete'>
-            <h1 className='titulo-delete'>Dar Baja Niños/as</h1>
+  const [selectedChild, setSelectedChild] = useState("");
 
-            <div className='contenedor-linea-delete'>
-                <hr className='linea-delete'></hr>
-            </div>
-            <Form.Group className="mb-3 mt-5">
-                <Form.Label className='mb-1'><b>Nombre</b></Form.Label>
-                <Form.Control type="text" placeholder="Ingrese un nombre"/>
-            </Form.Group>
+  useEffect(() => {
+    setSelectedChild(props.id);
+  }, []);
 
-            <div className='contenedor-boton-eliminar'>
-                <Button className='boton-eliminar mt-5' boton variant="primary">
-                    Eliminar
-                </Button>
-            </div>
+  async function handleDelete(event) {
+    event.preventDefault();
+    console.log(selectedCribroom);
+    try {
+      const payload = {
+        is_active: "false",
+      };
 
-        </Form>
-    );
+      console.log("making fetch");
+      const response = await axios.patch(
+        `/api/child/${selectedChild}/?delete`,
+        payload
+      );
+      props.onHide();
+    } catch (err) {
+      alert("Error al eliminar la sala cuna", err);
+    }
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <div>
+        <Modal.Title className="titulo-eliminar">
+          Eliminar Sala Cuna
+        </Modal.Title>
+      </div>
+      <div className="contenedor-linea-eliminar">
+        <hr className="linea-eliminar"></hr>
+      </div>
+      <div className="par">
+        <p>Esta seguro que desea Eliminar la Sala Cuna {selectedChild.name}?</p>
+        <p>Esto hara que su estado pase a ser Inactivo,</p>
+      </div>
+      <div className="par">
+        <Alert severity="warning">
+          <p>Los chicos que esten en esta sala cuna tambien</p>
+          <p>
+            pasaran a estar en <strong>estado Inactivo</strong>
+          </p>
+        </Alert>
+      </div>
+      <Modal.Footer>
+        <div className="button-wrapper">
+          <div>
+            <Button
+              className="mt-3"
+              boton
+              variant="danger"
+              onClick={handleDelete}
+            >
+              Deshabilitar
+            </Button>
+          </div>
+          <Button
+            className="mt-3"
+            boton
+            variant="primary"
+            onClick={props.onHide}
+          >
+            Cancelar
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 }
