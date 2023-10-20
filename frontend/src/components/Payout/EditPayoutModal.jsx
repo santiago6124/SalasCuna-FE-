0d2a2f5f3e576fd4b16bc06f5bc81e6a53b5766b
facuolivamar <facundoolivam@gmail.com>
2023-10-19 21:48:42 -0300

    EditPayoutModal dinamic render fields

--- frontend/src/components/Payout/EditPayoutModal.jsx
+++ frontend/src/components/Payout/EditPayoutModal.jsx
@@ -2,12 +2,98 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form/";
 import { Container } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import React, { useContext } from "react"; // Import useContext
+import React, { useContext, useState, useEffect } from "react"; // Import useContext
 import axios from "axios";
 import AuthContext from "../../context/AuthContext"; // Import the AuthContext
 
+
+import {
+  getAllZones,
+} from "../../api/salasCuna.api";
+
 export function EditPayout(props) {
   const { authTokens } = useContext(AuthContext); // Get authTokens from the AuthContext
+  const [zoneOptions, setZoneOptions] = useState([]);
+
+  const [formFields, setFormFields] = useState({
+    payout: [
+      {
+        name: "amount",
+        label: "Editar el monto del pago",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "date",
+        label: "Fecha",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "zone",
+        label: "Zona",
+        type: "select",
+        options: zoneOptions, // You need to define zoneOptions props.zones
+        required: true,
+      },
+    ],
+  });
+
+  const [formData, setFormData] = useState({});  /// mas adelante get request para obtener cribroom basado en los props id
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  useEffect(() => {
+    loadZones();
+  }, [props]);
+
+  async function loadZones() {
+    try {
+      const response = await getAllZones(props.tokens);
+      console.log('response: ', response);
+      setZoneOptions(response.data);
+      formFields['payout'][2]['options'] = response.data;
+    } catch (error) {
+      console.error("Error fetching zona options:", error);
+    }
+  }
+
+  
+  const renderFormFields = (fields) => {
+
+    return fields.map((field) => (
+      <Form.Group className="mb-3" key={field.name}>
+        <Form.Label className="mb-1">{field.label}</Form.Label>
+        {field.type === "select" ? (
+          <select
+            name={field.name}
+            value={formData[field.name]}
+            onChange={handleInputChange}
+            className="form-control"
+            required={field.required}
+          >
+            {field.options.map((option) => (
+              <option key={option.id} value={option.id}>
+                {option[Object.keys(option)[1]]} {/* Adjust this based on your option structure */}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <Form.Control
+            type={field.type}
+            placeholder={`Ingrese ${field.label.toLowerCase()}`}
+            name={field.name}
+            value={formData[field.name]}
+            onChange={handleInputChange}
+            required={field.required}
+          />
+        )}
+      </Form.Group>
+    ));
+  };
 
   async function handleEdit(event) {
     event.preventDefault();
@@ -26,7 +112,9 @@ export function EditPayout(props) {
     
     try {
       let response = await axios.put(`/api/payout/${props.id}/`, payload, { headers });
-      if (response.request.status === 201) {
+      // console.log(response.request.status);
+      // console.log(response.request);
+      if (response.request.statusText === 'OK') {
         console.log('Payout edited successfully');
         props.onHide();
       } else {
@@ -47,31 +135,8 @@ export function EditPayout(props) {
               <hr className="linea"></hr>
             </div>
 
-            <Form.Group className="mb-3">
-              <Form.Label className="mb-1">Monto</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Editar el monto del pago"
-                name="amount"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label className="mb-1">Fecha</Form.Label>
-              <Form.Control type="date" name="date" />
-            </Form.Group>
-            <div className="col-md-2">
-              <Form.Label className="mb-1">Seleccionar Zona</Form.Label>
-              <Form.Select as="select" name="zone">
-                <option value="" disabled>
-                  Seleccionar Zona
-                </option>
-                {props.zones.map((zone) => (
-                  <option key={zone.id} value={zone.id}>
-                    {zone.name}
-                  </option>
-                ))}
-              </Form.Select>
-            </div>
+            {renderFormFields(formFields.payout)}
+
             <div className="contenedor-boton-qr ">
               <Button
                 className="boton-edit mt-3"
