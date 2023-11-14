a29da7c82c867f0bde16da6123b3dd722a608f50
jose urtubey <joseaurtubey@gmail.com>
2023-11-14 10:14:09 -0300

    changed styles on profile page

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -1,51 +1,51 @@
 import "./Account.css";
-import {Row, Col, Form} from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 export default function ProfilePage() {
-    return (
-        <Form>
-                <Row className="d-flex justify-content-center">
-                    <Col className="justify-content-center text-center">
-                        <Form.Group>
-                            <Form.Label>Nombre</Form.Label>
-                            <Form.Control type="text" placeholder="Nombre"/>
-                        </Form.Group>
-                    </Col>
-                    <Col className="justify-content-center text-center">
-                    <Form.Group>
-                            <Form.Label>Apellido</Form.Label>
-                            <Form.Control type="text" placeholder="Apellido"/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="d-flex justify-content-center">
-                    <Col className="justify-content-center text-center">
-                        <Form.Group>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="example@gmail.com"/>
-                        </Form.Group>
-                    </Col>
-                    <Col className="justify-content-center text-center">
-                    <Form.Group>
-                            <Form.Label>DNI</Form.Label>
-                            <Form.Control type="text" placeholder="12345678"/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="d-flex justify-content-center">
-                    <Col className="justify-content-center text-center">
-                        <Form.Group>
-                            <Form.Label>Telefono</Form.Label>
-                            <Form.Control type="text" placeholder="54 9 351 123 4567"/>
-                        </Form.Group>
-                    </Col>
-                    <Col className="justify-content-center text-center">
-                    <Form.Group>
-                            <Form.Label>Rol</Form.Label>
-                            <Form.Control type="text" placeholder="Rol"/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-        </Form>
-    );
+  return (
+    <Form>
+      <Row className="d-flex justify-content-center mt-3">
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Nombre" />
+          </Form.Group>
+        </Col>
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Apellido" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center mt-5">
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="example@gmail.com" />
+          </Form.Group>
+        </Col>
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>DNI</Form.Label>
+            <Form.Control type="text" placeholder="12345678" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center mt-5 mb-4">
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>Telefono</Form.Label>
+            <Form.Control type="text" placeholder="54 9 351 123 4567" />
+          </Form.Group>
+        </Col>
+        <Col className="justify-content-center text-center">
+          <Form.Group>
+            <Form.Label>Rol</Form.Label>
+            <Form.Control type="text" placeholder="Rol" />
+          </Form.Group>
+        </Col>
+      </Row>
+    </Form>
+  );
 }
--- frontend/src/components/Account/SideMenu.jsx
+++ frontend/src/components/Account/SideMenu.jsx
@@ -1,17 +1,31 @@
 import "./Account.css";
-import profileImage from '../../media/Profile.jpg';
+import profileImage from "../../media/Profile.jpg";
 import { Button } from "react-bootstrap";
 
+import { Col, Row } from "react-bootstrap";
+
 export default function SideMenu() {
-    return(
-        <div className="MainDiv">
-            <div className="image-container">
-                <img src={profileImage} alt="Perfil" className="circle-image" />
-            </div>
-            <div className="contenedor-linea-sm">
-                <hr className="linea-sm"></hr>
-            </div>
-            <Button className="prueba">A VER</Button>
-        </div>   
-    );
+  return (
+    <div className="MainDiv">
+      <div className="image-container">
+        <img src={profileImage} alt="Perfil" className="circle-image" />
+      </div>
+      <div className="mt-2">
+        <h3>Bienvenido:</h3>
+      </div>
+      <div className="contenedor-linea-sm">
+        <hr className="linea-sm"></hr>
+      </div>
+      <Col className="mt-1 mb-2">
+        <Button variant="outline-primary" className="prueba">
+          Info de cuenta
+        </Button>
+      </Col>
+      <Col className="mt-1 mb-2">
+        <Button variant="outline-primary" className="prueba">
+          Cambiar contrasenia
+        </Button>
+      </Col>
+    </div>
+  );
 }
