001f2e26cf5ce0131504f7e1f7af3ee7f380fea3
jose urtubey <joseaurtubey@gmail.com>
2023-11-17 11:57:18 -0300

    added more fields and centered text in the timeline card

--- frontend/src/components/CribroomDashboard/ObjectHistory.jsx
+++ frontend/src/components/CribroomDashboard/ObjectHistory.jsx
@@ -6,6 +6,9 @@ import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+import { Paper } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Row, Col } from "react-bootstrap";
 import Cookies from "js-cookie";
 import axios from "axios";
 
@@ -42,16 +45,53 @@ export default function HistoryTimeline(props) {
     }
   }
 
+  const getHistoryTypeString = (type) => {
+    switch (type) {
+      case "+":
+        return "Creado";
+      case "-":
+        return "Eliminado";
+      case "~":
+        return "Editado";
+      default:
+        return "Razon desconocida";
+    }
+  };
+
   // Check if cribroom.history is defined before mapping
-  const historyItems = cribroom.history && cribroom.history.map((event, index) => (
-    <TimelineItem key={index}>
-      <TimelineSeparator>
-        <TimelineDot />
-        {index < cribroom.history.length - 1 && <TimelineConnector />}
-      </TimelineSeparator>
-      <TimelineContent>Nombre: {event.name}</TimelineContent>
-    </TimelineItem>
-  ));
+  const historyItems =
+    cribroom.history &&
+    cribroom.history.map((event, index) => (
+      <TimelineItem key={index}>
+        <TimelineSeparator>
+          <TimelineDot />
+          {index < cribroom.history.length - 1 && <TimelineConnector />}
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper className="p-1 h-80 w-100 card-text">
+            <Row className="ms-1 ">
+              <Typography variant="h8" component="h8" className=" ms-1 ">
+                Nombre: {event.name}
+              </Typography>
+            </Row>
+            <Row className="ms-1">
+              <Typography
+                variant="h8"
+                component="h8"
+                className="justify-content-left ms-1"
+              >
+                Fecha: {event.history_date}
+              </Typography>
+            </Row>
+            <Row className="ms-1">
+              <Typography variant="h8" component="h8" className=" ms-1 ">
+                Tipo: {getHistoryTypeString(event.history_type)}
+              </Typography>
+            </Row>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+    ));
 
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
@@ -63,4 +103,3 @@ export default function HistoryTimeline(props) {
     </Modal>
   );
 }
-
