96fca2bbaa90f2ef32992be51d2384785fe39afc
facuolivamar <facundoolivam@gmail.com>
2023-11-27 10:28:28 -0300

    fixed export padron and it works almost perfect

--- frontend/src/components/CribroomDashboard/ExcelExport/ExportMenuItem.jsx
+++ frontend/src/components/CribroomDashboard/ExcelExport/ExportMenuItem.jsx
@@ -53,86 +53,138 @@ async function handleExport(apiRef, selectedCribroomId, authTokens) {
   //   console.error("An error occurred (child request):", error);
   // }
 
-  var rowValuesDict = {
-    // '1': ['Sala Cuna:', cribroomResponse['code']],
-    // '2': [cribroomResponse['name']],
-    '3': ['Mes', 'generate'],
-    '4': ['Año', 'generate', '', '', '', '', '', '', '', '', '', '', '', '', '', 'CANTIDAD DE NIÑOS', 'amount'],
-    '5': ['Provincia de Córdoba'],
-    // '6': [cribroomResponse['entity']],
-    '7': [''],
-    '8': [''],
-    '9':[
-      'N°',
-      'SALA CUNA',
-      'APELLIDO',
-      'NOMBRE',
-      'N° DNI',
-      'FECHA DE NACIMIENTO',
-      'EDAD',
-      'SEXO',
-      'CALLE',
-      'NUMERO',
-      'DEPARTAMENTO',
-      'LOCALIDAD',
-      'CARACTERISTICA TELEFONICA',
-      'TELEFONO',
-      'APELLIDO Y NOMBRE MADRE',
-      'DNI',
-      'TURNO',
-      'ESTADO',
-    ],
-  }
+  console.log('selectedCribroomId: ', selectedCribroomId);
+  console.log('authTokens: ', authTokens);
 
   const data = getExcelData(apiRef);
 
-  const rows = data.map((row) => {
-    const mRow = {};
-    for (const key of config.keys) {
-      mRow[key] = row[key];
+  // const rows = data.map((row) => {
+  //   const mRow = {};
+  //   for (const key of config.keys) {
+  //     mRow[key] = row[key];
+  //   }
+  //   return mRow;
+  // });
+  console.log('rows: ', data);
+
+  const monthNames = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ];
+  const currentDate = new Date();
+
+  const currentMonth = monthNames[currentDate.getMonth()];
+  const currentYear = currentDate.getFullYear();
+
+  // Use a for...of loop instead of forEach
+  for (const item of selectedCribroomId) {
+    
+    try {
+      var cribroomResponse = await cribroom_request(authTokens.access, 'get', 1, {}, item);
+      console.log('cribroomResponse: ', cribroomResponse);
+    } catch (error) {
+      console.error("An error occurred (child request):", error);
     }
-    return mRow;
-  });
-  console.log('rows: ', rows);
 
-  // Create a new workbook
-  const workbook = XLSX.utils.book_new();
-
-  // Create a new worksheet
-  const worksheet = XLSX.utils.aoa_to_sheet([]); // An empty worksheet
-
-  // Populate the worksheet with values from rowValuesDict
-  let startRow = 1; // Starting row to add values
-  for (const rowNum in rowValuesDict) {
-    if (rowValuesDict.hasOwnProperty(rowNum)) {
-      const rowData = rowValuesDict[rowNum];
-      XLSX.utils.sheet_add_aoa(worksheet, [rowData], { origin: `A${startRow}` });
-      startRow++;
+    var rowValuesDict = {
+      '1': ['Sala Cuna:', cribroomResponse.data.name, 'Codigo:', cribroomResponse.data.code, 'Entidad:', cribroomResponse.data.entity],
+      '2': ['Capacidad maxima:', cribroomResponse.data.max_capacity, 'Niños activos:', cribroomResponse.data.actualCapacity],
+      '3': ['Mes', currentMonth],
+      '4': ['Año', currentYear],
+      '5': ['Provincia de Córdoba'],
+      // '6': [cribroomResponse['entity']],
+      '7': [''],
+      '8': [''],
+      '9':[
+        'ID',
+        'SALA CUNA',
+        'APELLIDO',
+        'NOMBRE',
+        'IDENTIFICACION',
+        'TIPO IDENTIFICACION',
+        'FECHA DE NACIMIENTO',
+        'EDAD',
+        'SEXO',
+        'CALLE',
+        'NUMERO',
+        'LOCALIDAD',
+        // 'CARACTERISTICA TELEFONICA',
+        // 'TELEFONO',
+        'APELLIDO TUTOR',
+        'NOMBRE TUTOR',
+        'IDENTIFICACION TUTOR',
+        'TURNO',
+        'ESTADO',
+      ],
     }
+
+    try {
+      var childResponse = await child_request(authTokens.access, 'get', 1, {}, undefined, `&cribroom_id=${item}`);
+      console.log('childResponse: ', childResponse);
+    } catch (error) {
+      console.error("An error occurred (child request):", error);
+    }
+
+    var index = 10
+    for (const child of childResponse.data){
+      rowValuesDict[index] = [
+        child.id,
+        cribroomResponse.data.name,
+        child.id,
+        child.first_name,
+        child.last_name,
+        child.identification,
+        child.ident_type.type,
+        child.birthdate,
+        child.age,
+        child.gender.gender,
+        child.street,
+        child.house_number,
+        child.locality.locality,
+        child.guardian.last_name,
+        child.guardian.identification,
+        child.shift.name,
+        child.is_active === true ? 'Activo' : 'Inactivo',
+      ];
+      index += 1;
+    }
+
+    // Create a new workbook
+    const workbook = XLSX.utils.book_new();
+    // Create a new worksheet
+    const worksheet = XLSX.utils.aoa_to_sheet([]); // An empty worksheet
+    // Populate the worksheet with values from rowValuesDict
+    let startRow = 1; // Starting row to add values
+    for (const rowNum in rowValuesDict) {
+      if (rowValuesDict.hasOwnProperty(rowNum)) {
+        const rowData = rowValuesDict[rowNum];
+        XLSX.utils.sheet_add_aoa(worksheet, [rowData], { origin: `A${startRow}` });
+        startRow++;
+      }
+    }
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Metadata');
+    XLSX.writeFile(workbook, `${cribroomResponse.data.name} ${cribroomResponse.data.code} ${currentMonth}${currentYear}.xlsx`, { compression: true });
   }
-
-  // Add the worksheet to the workbook
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Metadata');
-
-  // Trigger the file download
-  XLSX.writeFile(workbook, config.fileName, { compression: true });
+  
 }
 
 export function ExportMenuItem(props) {
   const apiRef = useGridApiContext();
   const { hideMenu } = props;
 
-  const { selectedCribroomId } = props;
+  const { selectedCribroomId, authTokens  } = props;
 
   return (
     <MenuItem
       onClick={() => {
-        handleExport(apiRef, selectedCribroomId);
+        handleExport(apiRef, selectedCribroomId, authTokens);
         // Hide the export menu after the export
         hideMenu?.();
       }}
     >
-      Download Excel
+      Descargar Padron
     </MenuItem>
   );
 }
